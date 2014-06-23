ecf322a320615055b61d0b51e3aa0a80bf07a892
Caridy Patino <caridy@gmail.com>
2014-06-23 15:12:07 -0400

    adding steps to render on the server side

--- example/render.js
+++ example/render.js
@@ -1,5 +1,8 @@
 // this script is meant to run in nodejs to output the rendered content on the server side
 
+// IMPORTANT: make sure you install the jsx CLI: `npm install -g react-tools`, and
+//            run `jsx src/ build/` within the `example/` folder to render the content on the server side.
+
 // making React a global variable on the server side
 global.React = require('react');
 // requiring the Intl mixin
