ff43034aca250df736890f9e6a5d0cf965175878
Juan Ignacio Dopazo <juan@dopazo.me>
2014-10-01 20:03:31 -0300

    Add Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,6 +111,83 @@ module.exports = function (grunt) {
                     ]
                 }
             }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    base: '.',
+                    port: 9999
+                }
+            }
+        },
+
+        'saucelabs-mocha': {
+            all: {
+                options: {
+                    urls: ['http://127.0.0.1:9999/tests/smoke/index.html'],
+                    build: process.env.TRAVIS_BUILD_NUMBER,
+                    sauceConfig: {
+                        'record-video': false,
+                        'capture-html': false,
+                        'record-screenshots': false,
+                        'command-timeout': 60
+                    },
+                    throttled: 3,
+                    browsers: [
+                        {
+                            browserName: 'internet explorer',
+                            platform: 'Windows XP',
+                            version: '7'
+                        },
+                        {
+                            browserName: 'internet explorer',
+                            platform: 'Windows 7',
+                            version: '8'
+                        },
+                        {
+                            browserName: 'internet explorer',
+                            platform: 'Windows 7',
+                            version: '9'
+                        },
+                        {
+                            browserName: 'internet explorer',
+                            platform: 'Windows 8',
+                            version: '10'
+                        },
+                        {
+                            browserName: 'internet explorer',
+                            platform: 'Windows 8.1',
+                            version: '11'
+                        },
+                        {
+                            browserName: 'chrome',
+                            platform: 'Windows 7',
+                            version: '37'
+                        },
+                        {
+                            browserName: 'firefox',
+                            platform: 'Windows 7',
+                            version: '32'
+                        },
+                        {
+                            browserName: 'iphone',
+                            platform: 'OS X 10.9',
+                            version: '7.1'
+                        },
+                        {
+                            browserName: 'android',
+                            platform: 'Linux',
+                            version: '4.4'
+                        },
+                        {
+                            browserName: 'safari',
+                            platform: 'OS X 10.9',
+                            version: '7'
+                        }
+                    ]
+                }
+            }
         }
     });
 
@@ -121,6 +198,13 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-bundle-jsnext-lib');
     grunt.loadNpmTasks('grunt-extract-cldr-data');
+    grunt.loadNpmTasks('grunt-saucelabs');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('sauce', [
+        'connect',
+        'saucelabs-mocha'
+    ]);
 
     grunt.registerTask('cldr', ['extract_cldr_data']);
 
