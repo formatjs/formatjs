8985b23f823b18475616e90f7c70899bbe3c8f90
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-10-22 17:16:05 -0400

    Fix cwd path in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
         copy: {
             tmp: {
                 expand: true,
-                cwd   : 'src/',
+                cwd   : 'tmp/src/',
                 src   : '**/*.js',
                 dest  : 'lib/'
             }
