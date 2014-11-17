dce2e0ed3a6be3aacb0f9d137a4bd158701c203e
Eric Ferraiuolo <edf@ericf.me>
2014-11-17 10:39:14 -0500

    Update smoke tests for React 0.12 and components

--- tests/smoke/smoke.js
+++ tests/smoke/smoke.js
@@ -1,11 +1,11 @@
-/*global describe, it, expect, React, ReactIntlMixin*/
+/*global describe, it, expect, React, ReactIntl*/
 describe('React Intl mixin', function () {
 
     it('Formats numbers correctly', function () {
         var FormattedNumberComponent = React.createClass({
             displayName: 'FormattedNumber',
 
-            mixins: [ReactIntlMixin],
+            mixins: [ReactIntl.Mixin],
 
             render: function () {
                 return React.DOM.div(null, this.formatNumber(1000));
@@ -22,11 +22,22 @@ describe('React Intl mixin', function () {
         expect(testNode.firstChild.innerHTML).to.equal('1.000');
     });
 
+    it('Formats numbers correctly using IntlNumber component', function () {
+        var IntlNumber = React.createFactory(ReactIntl.Number);
+        var testNode = document.getElementById('test1');
+
+        React.render(IntlNumber({
+            locales: ['es-AR']
+        }, 1000), testNode);
+
+        expect(testNode.firstChild.innerHTML).to.equal('1.000');
+    });
+
     it('Formats dates correctly', function () {
         var FormattedDateComponent = React.createClass({
             displayName: 'FormattedDate',
 
-            mixins: [ReactIntlMixin],
+            mixins: [ReactIntl.Mixin],
 
             render: function () {
                 return React.DOM.div(null, this.formatDate(this.props.date, {
@@ -54,11 +65,32 @@ describe('React Intl mixin', function () {
         expect(testNode.firstChild.innerHTML).to.contain('Thursday');
     });
 
+    it('Formats dates correctly using IntlDate component', function () {
+        var IntlDate = React.createFactory(ReactIntl.Date);
+        var testNode = document.getElementById('test2');
+
+        React.render(IntlDate({
+            locales: ['es-AR'],
+            weekday: 'long',
+            timeZone: 'UTC'
+        }, Date.UTC(2014, 8, 22, 0, 0, 0, 0)), testNode);
+
+        expect(testNode.firstChild.innerHTML).to.contain('lunes');
+
+        React.render(IntlDate({
+            locales: ['en-US'],
+            weekday: 'long',
+            timeZone: 'UTC'
+        }, 0), testNode);
+
+        expect(testNode.firstChild.innerHTML).to.contain('Thursday');
+    });
+
     it('Formats messages correctly', function () {
         var FormattedMessageComponent = React.createClass({
             displayName: 'FormattedMessage',
 
-            mixins: [ReactIntlMixin],
+            mixins: [ReactIntl.Mixin],
 
             getMyMessage: function () {
                 return 'You have {numPhotos, plural, =0 {no photos} =1 {one photo} other {# photos}}.';
@@ -78,6 +110,18 @@ describe('React Intl mixin', function () {
             locales: ['en-US'],
             numPhotos: 1
         }), testNode);
+
+        expect(testNode.firstChild.innerHTML).to.equal('You have one photo.');
+    });
+
+    it('Formats messages correctly using IntlMessage component', function () {
+        var IntlMessage = React.createFactory(ReactIntl.Message);
+        var testNode = document.getElementById('test3');
+
+        React.render(IntlMessage({
+            locales: ['en-US'],
+            numPhotos: 1
+        }, 'You have {numPhotos, plural, =0 {no photos} =1 {one photo} other {# photos}}.'), testNode);
 
         expect(testNode.firstChild.innerHTML).to.equal('You have one photo.');
     });
