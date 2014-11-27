01f65101f205a1c4d935e72d1c00c83be470edd8
Santiago Aguiar <santiago.aguiar@gmail.com>
2014-11-26 22:28:32 -0200

    Added entry point for browser bundles

---
+++ browser.js
@@ -0,0 +1,18 @@
+// Entry point for browser bundles.
+
+/* jshint node:true */
+'use strict';
+
+// Expose `React` as a global, because the ReactIntlMixin assumes it's global.
+var oldReact = global.React;
+global.React = require('react');
+
+exports = module.exports = require('./lib/mixin').default;
+Object.defineProperty(exports, 'default', {value: exports});
+
+// Put back `global.React` to how it was.
+if (oldReact) {
+    global.React = oldReact;
+} else {
+    delete global.React;
+}
