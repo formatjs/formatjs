60bdb48439ebed14ab22683d8d1a5d13d7d4dafe
Eric Ferraiuolo <edf@ericf.me>
2014-12-08 10:30:08 -0500

    Improve browser bundling with Browserify/Webpack
    This adds a `"browser"` field to the `package.json` file to hint to
    both Browserify and Webpack that they should _not_ include the data for
    all locales (just English) when bundling for the browser.

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
 var oldReact = global.React;
 global.React = require('react');
 
-// Add all locale data to `ReactIntlMixin`.
+// Add all locale data to `ReactIntlMixin`. This module will be ignored when
+// bundling for the browser with Browserify/Webpack.
 require('./lib/locales');
 
-exports = module.exports = require('./lib/mixin').default;
+exports = module.exports = require('./lib/main').default;
 Object.defineProperty(exports, 'default', {value: exports});
 
 // Put back `global.React` to how it was.
