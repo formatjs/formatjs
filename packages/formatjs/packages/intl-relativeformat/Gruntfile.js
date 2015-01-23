1f8f926aa3a25bb476b16b7ad8085d0e203c963d
Eric Ferraiuolo <edf@ericf.me>
2015-01-23 12:05:52 -0500

    Upgrade to es6-module-transpiler@0.10.0
    Upgrades to the latest version of the transpiler which doesn't use
    dot-notation when accessing imports/exports. The generated
    `lib/locales.js` file was also update to no use `.default`.

--- packages/formatjs/packages/intl-relativeformat/Gruntfile.js
+++ packages/formatjs/packages/intl-relativeformat/Gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function (grunt) {
                 options: {
                     prelude: [
                         '// GENERATED FILE',
-                        'var IntlRelativeFormat = require("./core").default;\n\n'
+                        'var IntlRelativeFormat = require("./core")["default"];\n\n'
                     ].join('\n'),
 
                     wrapEntry: function (entry) {
