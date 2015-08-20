fce1b66cf262b46cac61f6f8c7162f025d44e01d
Eric Ferraiuolo <edf@ericf.me>
2015-08-20 15:59:13 -0400

    Add two new example apps: Hello World, Translations

---
+++ examples/translations/src/client/components/greeting.js
@@ -0,0 +1,39 @@
+import React, {Component, PropTypes} from 'react';
+import {FormattedMessage, FormattedNumber, FormattedRelative} from 'react-intl';
+
+class Greeting extends Component {
+    render() {
+        const {user} = this.props;
+
+        return (
+            <p>
+                <FormattedMessage
+                    id="greeting.welcome_message"
+                    defaultMessage={`
+                        Welcome {name}, you have received {unreadCount, plural,
+                            =0 {no new messages}
+                            one {{formattedUnreadCount} new messages}
+                            other {{formattedUnreadCount} new messages}
+                        } since {formattedLastLoginTime}.
+                    `}
+                    values={{
+                        name: <b>{user.name}</b>,
+                        unreadCount: user.unreadCount,
+                        formattedUnreadCount: (
+                            <b><FormattedNumber value={user.unreadCount} /></b>
+                        ),
+                        formattedLastLoginTime: (
+                            <FormattedRelative value={user.lastLoginTime} />
+                        ),
+                    }}
+                />
+            </p>
+        );
+    }
+}
+
+Greeting.propTypes = {
+    user: PropTypes.object.isRequired,
+};
+
+export default Greeting;
---
+++ examples/translations/src/client/components/locales-menu.js
@@ -0,0 +1,36 @@
+import React, {Component, PropTypes} from 'react'
+import {FormattedMessage, intlContextTypes, formatMessage} from 'react-intl';
+
+class LocalesMenu extends Component {
+    render() {
+        const {intl} = this.context;
+
+        let enUSDescription = formatMessage(intl, {
+            id: 'menu.item_en_us_description',
+            defaultMessage: 'The default locale of this example app.',
+        });
+
+        let enUPPERDescription = formatMessage(intl, {
+            id: 'menu.item_en_upper_description',
+            defaultMessage: 'The fake, all uppercase "locale" for this example app.',
+        });
+
+        return (
+            <menu>
+                <li>
+                    <a href="/?locale=en-US" title={enUSDescription}>en-US</a>
+                </li>
+
+                <li>
+                    <a href="/?locale=en-UPPER" title={enUPPERDescription}>en-UPPER</a>
+                </li>
+            </menu>
+        );
+    }
+}
+
+LocalesMenu.contextTypes = {
+    intl: PropTypes.shape(intlContextTypes).isRequired,
+};
+
+export default LocalesMenu;
