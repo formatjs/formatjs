69bb47530a9945f6bd177d16b844765912381862
Eric Ferraiuolo <edf@ericf.me>
2015-08-21 17:58:56 -0400

    Simplify Hello World example and add it to the README

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import {IntlProvider, FormattedNumber, FormattedPlural} from 'react-intl';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name       : 'Eric',
+            unreadCount: 1000,
+        };
+    }
+
+    render() {
+        return (
+            <p>
+                Hello <b>{this.state.name}</b>, you have {' '}
+                <FormattedNumber value={this.state.unreadCount} />
+                <FormattedPlural value={this.state.unreadCount}
+                    one=" unread message."
+                    other=" unread messages."
+                />
+            </p>
+        );
+    }
+}
 
 ReactDOM.render(
-    <App />,
+    <IntlProvider>
+        <App />
+    </IntlProvider>,
     document.getElementById('container')
 );
