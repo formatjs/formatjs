107c19530cb8f5cca997c5f7bc6f91f185d5b9c8
Eric Ferraiuolo <edf@ericf.me>
2015-09-11 18:00:38 -0400

    Add TODO comment about defineMessages()

--- src/react-intl.js
+++ src/react-intl.js
@@ -20,6 +20,7 @@ export {default as FormattedHTMLMessage} from './components/html-message';
 
 export {intlShape} from './types';
 
+// TODO Should there be a `defineMessages()` function that takes a collection?
 export function defineMessage(messageDescriptor) {
     // TODO: Type check in dev? Return something different?
     return messageDescriptor;
