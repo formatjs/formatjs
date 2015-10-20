e812c864befe49e6d7df52debcb09d353de151ef
Eric Ferraiuolo <edf@ericf.me>
2015-10-20 00:25:34 -0400

    Fix Translations example for locale data check

--- examples/translations/src/client/index.js
+++ examples/translations/src/client/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {IntlProvider} from 'react-intl';
+import {addLocaleData, IntlProvider} from 'react-intl';
+import enLocaleData from 'react-intl/locale-data/en';
 import App from './components/app';
+
+// This example app only uses English. A fake `"en-UPPER"` locale is created so
+// translations can be emulated.
+addLocaleData(enLocaleData);
+addLocaleData({
+    locale: 'en-UPPER',
+    parentLocale: 'en',
+});
 
 const {locale, messages} = window.App;
 
