97c5c39e92132c68f1099f410ea3787d173cf8d7
Eric Ferraiuolo <edf@ericf.me>
2015-11-11 00:49:35 -0500

    Tweak internal binding identifier naming

--- src/react-intl-with-locales.js
+++ src/react-intl-with-locales.js
@@ -4,9 +4,9 @@
  * See the accompanying LICENSE file for terms.
  */
 
-import allLocales from './locale-data/index';
+import allLocaleData from './locale-data/index';
 import {addLocaleData} from './react-intl';
 
 export * from './react-intl';
 
-addLocaleData(allLocales);
+addLocaleData(allLocaleData);
