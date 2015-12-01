4acf298bf5799b07b9f00449d354453cbcdead99
Eric Ferraiuolo <edf@ericf.me>
2015-12-01 14:01:28 -0500

    Update react-intl-with-locales unit tests
    This makes the tests more accurate by comparing them with the actual
    exports of the main React Intl module (without locale data).
    
    Right now there's a bug in Babel that needs to be fixed, so the test
    is being skipped: https://github.com/babel/babel/pull/3120

--- test/unit/react-intl-with-locales.js
+++ test/unit/react-intl-with-locales.js
@@ -1,57 +1,12 @@
 import expect from 'expect';
-import * as ReactIntl from '../../src/react-intl-with-locales';
+import * as ReactIntl from '../../src/react-intl';
+import * as ReactIntlWL from '../../src/react-intl-with-locales';
 
 describe('react-intl-with-locales', () => {
     describe('exports', () => {
-        it('exports `addLocaleData`', () => {
-            expect(ReactIntl.addLocaleData).toBeA('function');
-        });
-
-        it('exports `defineMessages`', () => {
-            expect(ReactIntl.defineMessages).toBeA('function');
-        });
-
-        it('exports `injectIntl`', () => {
-            expect(ReactIntl.injectIntl).toBeA('function');
-        });
-
-        describe('React Components', () => {
-            it('exports `IntlProvider`', () => {
-                expect(ReactIntl.IntlProvider).toBeA('function');
-            });
-            
-            it('exports `FormattedDate`', () => {
-                expect(ReactIntl.FormattedDate).toBeA('function');
-            });
-
-            it('exports `FormattedTime`', () => {
-                expect(ReactIntl.FormattedTime).toBeA('function');
-            });
-
-            it('exports `FormattedRelative`', () => {
-                expect(ReactIntl.FormattedRelative).toBeA('function');
-            });
-
-            it('exports `FormattedNumber`', () => {
-                expect(ReactIntl.FormattedNumber).toBeA('function');
-            });
-
-            it('exports `FormattedPlural`', () => {
-                expect(ReactIntl.FormattedPlural).toBeA('function');
-            });
-
-            it('exports `FormattedMessage`', () => {
-                expect(ReactIntl.FormattedMessage).toBeA('function');
-            });
-
-            it('exports `FormattedHTMLMessage`', () => {
-                expect(ReactIntl.FormattedHTMLMessage).toBeA('function');
-            });
-        });
-
-        describe('PropTypes Definitions', () => {
-            it('exports `intlShape`', () => {
-                expect(ReactIntl.intlShape).toBeAn('function');
+        it('has the same exports as "react-intl"', () => {
+            Object.keys(ReactIntl).forEach((namedExport) => {
+                expect(ReactIntlWL[namedExport]).toBe(ReactIntl[namedExport]);
             });
         });
     });
