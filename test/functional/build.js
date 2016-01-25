2fd2e585314329b9c1dcd4b62c51ac8375498355
Eric Ferraiuolo <edf@ericf.me>
2016-01-25 12:58:27 -0500

    Add basic functional tests per build

---
+++ test/functional/build.js
@@ -0,0 +1,18 @@
+import expect from 'expect';
+import * as ReactIntl from '../../src/';
+
+export default function (buildPath) {
+    describe('build', () => {
+        it('evaluates', () => {
+            expect(require(buildPath)).toExist();
+        });
+
+        it('has all React Intl exports', () => {
+            const ReactIntlBuild = require(buildPath);
+
+            Object.keys(ReactIntl).forEach((name) => {
+                expect(ReactIntlBuild[name]).toBeA(typeof ReactIntl[name]);
+            });
+        });
+    });
+}
---
+++ test/functional/format.js
@@ -0,0 +1,97 @@
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+import React from 'react';
+import {createRenderer} from 'react-addons-test-utils';
+
+expect.extend(expectJSX);
+
+export default function (ReactIntl) {
+    describe('format', () => {
+        const {
+            IntlProvider,
+            FormattedDate,
+            FormattedTime,
+            FormattedRelative,
+            FormattedNumber,
+            FormattedMessage,
+        } = ReactIntl;
+
+        let renderer;
+        let intlProvider;
+
+        beforeEach(() => {
+            renderer     = createRenderer();
+            intlProvider = new IntlProvider({locale: 'en'}, {});
+        });
+
+        it('formats dates', () => {
+            const date = new Date();
+            const el   = <FormattedDate value={date} month="numeric" />;
+
+            renderer.render(el, intlProvider.getChildContext());
+            expect(renderer.getRenderOutput()).toEqualJSX(
+                <span>{date.getMonth() + 1}</span>
+            );
+        });
+
+        it('formats times', () => {
+            const date = new Date();
+            const el   = <FormattedTime value={date} />;
+
+            const hours   = date.getHours();
+            const minutes = date.getMinutes();
+
+            renderer.render(el, intlProvider.getChildContext());
+            expect(renderer.getRenderOutput()).toEqualJSX(
+                <span>
+                    {`${hours > 12 ? (hours % 12) : hours}:${minutes} ${hours < 12 ? 'AM' : 'PM'}`}
+                </span>
+            );
+        });
+
+        it('formats dates relative to "now"', () => {
+            const now = Date.now();
+            const el  = <FormattedRelative value={now - 1000} initialNow={now} />;
+
+            renderer.render(el, intlProvider.getChildContext());
+            expect(renderer.getRenderOutput()).toEqualJSX(
+                <span>1 second ago</span>
+            );
+        });
+
+        it('formats numbers with thousands separators', () => {
+            const el = <FormattedNumber value={1000} />;
+
+            renderer.render(el, intlProvider.getChildContext());
+            expect(renderer.getRenderOutput()).toEqualJSX(
+                <span>1,000</span>
+            );
+        });
+
+        it('formats numbers with decimal separators', () => {
+            const el = <FormattedNumber value={0.1} minimumFractionDigits={2} />;
+
+            renderer.render(el, intlProvider.getChildContext());
+            expect(renderer.getRenderOutput()).toEqualJSX(
+                <span>0.10</span>
+            );
+        });
+
+        it('pluralizes labels in strings', () => {
+            const el = (
+                <FormattedMessage
+                    id="num_emails"
+                    defaultMessage="You have {emails, plural, one {# email} other {# emails}}."
+                    values={{
+                        emails: 1000,
+                    }}
+                />
+            );
+
+            renderer.render(el, intlProvider.getChildContext());
+            expect(renderer.getRenderOutput()).toEqualJSX(
+                <span>You have 1,000 emails.</span>
+            );
+        });
+    });
+}
