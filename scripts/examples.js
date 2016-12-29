493253c447bd7005da1c7ed38ed16ce54f1f530e
Eric Ferraiuolo <edf@ericf.me>
2016-12-28 22:54:30 -0500

    Add script to link local react-intl in examples/ (#794)

---
+++ scripts/examples.js
@@ -0,0 +1,16 @@
+import {sync as globSync} from 'glob';
+import {spawnSync} from 'child_process';
+
+const [command, ...args] = process.argv.slice(2);
+
+globSync('./examples/*/').forEach((exampleDir) => {
+    const opts = {
+        cwd: exampleDir,
+        stdio: 'inherit',
+    };
+
+    const result = spawnSync(command, args, opts);
+    if (result.status !== 0) {
+        throw new Error('Linking examples exited with non-zero');
+    }
+});
