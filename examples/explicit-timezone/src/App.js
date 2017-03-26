74c850520e25d041bcd2d5bfdd17cf9aa21f7219
Bryan Richards <brichardssa@gmail.com>
2017-03-26 14:35:23 +0200

    Make explicit timezone example more obvious

--- examples/explicit-timezone/src/App.js
+++ examples/explicit-timezone/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {FormattedDate, FormattedTime} from 'react-intl';
 
-export default (props) => <p>
-    The date in Tokyo is: <FormattedDate value={props.currentTime} />
-    <br />
-    The time in Tokyo is: <FormattedTime value={props.currentTime} />
-</p>;
+export default (props) => {
+    return <span>
+        <FormattedDate value={props.currentTime} />
+        &nbsp;
+        <FormattedTime value={props.currentTime} />
+    </span>;
+};
--- examples/explicit-timezone/src/index.js
+++ examples/explicit-timezone/src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from 'react-dom';
 import {IntlProvider} from 'react-intl';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.render(<div>
+    Timezone Explicitly set to Asia/Tokyo:
+    &nbsp;
     <IntlProvider locale="en" timeZone="Asia/Tokyo">
         <App currentTime={new Date()} />
-    </IntlProvider>,
-    document.getElementById('root')
+    </IntlProvider>
+    <br />
+    Timezone Explicitly set to UTC:
+    &nbsp;
+    <IntlProvider locale="en" timeZone="UTC">
+        <App currentTime={new Date()} />
+    </IntlProvider>
+    <br />
+    Timezone NOT Explicitly Set:
+    &nbsp;
+    <IntlProvider locale="en">
+        <App currentTime={new Date()} />
+    </IntlProvider>
+</div>,
+document.getElementById('root')
 );
