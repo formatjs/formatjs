16b3686e393b7c398b89b13661e881671af1b02d
Bryan Richards <brichardssa@gmail.com>
2017-03-26 11:10:02 +0200

    Fix up explicit timezone example

--- examples/explicit-timezone/src/index.js
+++ examples/explicit-timezone/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
     <IntlProvider locale="en" timeZone="Asia/Tokyo">
-        <App />
+        <App currentTime={new Date()} />
     </IntlProvider>,
     document.getElementById('root')
 );
