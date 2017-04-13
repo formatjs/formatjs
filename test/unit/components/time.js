77ea4b8c7c5cd97c0e4c6ebe9f48507e82fc76bc
Sylvester Keil <sylvester@keil.or.at>
2017-04-13 11:09:58 +0200

    Make tests work with latest React 15.5
    React 15.5 and later exports `createRenderer` via 'react-test-renderer'
    while React 14 uses 'react-addons-test-utils'. Because of the static
    nature of ES6 import statements we cannot conditionally import from
    different modules depending on React versions; therefore, this commit
    adds an NPM pretest script to generate 'test/renderer.js' which simply
    imports the right module for the current React version. This is to make
    sure tests work with both `npm run react:14` and `npm run react:15`, but
    ideally should be dropped once support for React 14 is dropped.

--- test/unit/components/time.js
+++ test/unit/components/time.js
@@ -1,7 +1,7 @@
 import expect, {spyOn} from 'expect';
 import expectJSX from 'expect-jsx';
 import React from 'react';
-import {createRenderer} from 'react-addons-test-utils';
+import {createRenderer} from '../../renderer';
 import IntlProvider from '../../../src/components/provider';
 import FormattedTime from '../../../src/components/time';
 
