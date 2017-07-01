5e8b904ce4001fb1bc5f568b1a27e702ecc7c8b9
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-07-01 13:00:55 -0700

    Merge pull request #156 from cwtuan/master
    fix #154 values for 0

--- packages/formatjs/packages/intl-messageformat/src/compiler.js
+++ packages/formatjs/packages/intl-messageformat/src/compiler.js
@@ -156,7 +156,7 @@ function StringFormat(id) {
 }
 
 StringFormat.prototype.format = function (value) {
-    if (!value) {
+    if (typeof value === 'undefined' || value === null) {
         return '';
     }
 
