747c5308084749a7087775e165a24cc14613d04d
Ryuichi Okumura <okuryu@okuryu.com>
2017-07-04 19:43:11 +0900

    Merge pull request #162 from yahoo/number-filter-stringformat
    zero values should not return an empty string

--- packages/formatjs/packages/intl-messageformat/src/compiler.js
+++ packages/formatjs/packages/intl-messageformat/src/compiler.js
@@ -156,7 +156,7 @@ function StringFormat(id) {
 }
 
 StringFormat.prototype.format = function (value) {
-    if (!value) {
+    if (!value && typeof value !== 'number') {
         return '';
     }
 
--- packages/formatjs/packages/intl-messageformat/tests/index.js
+++ packages/formatjs/packages/intl-messageformat/tests/index.js
@@ -110,6 +110,26 @@ describe('IntlMessageFormat', function () {
 
             expect(output).to.equal('My name is Anthony Pipkin.');
         });
+
+        it('should not ignore zero values', function() {
+            var mf = new IntlMessageFormat('I am {age} years old.');
+            var output = mf.format({
+                age: 0
+            });
+
+            expect(output).to.equal('I am 0 years old.');
+        });
+
+        it('should ignore false, null, and undefined', function() {
+            var mf = new IntlMessageFormat('{a}{b}{c}');
+            var output = mf.format({
+                a: false,
+                b: null,
+                c: undefined
+            });
+
+            expect(output).to.equal('');
+        });
     });
 
     describe('and plurals under the Arabic locale', function () {
