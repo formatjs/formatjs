41c2d943736fbf9c7aec2dc4c4fb415f0c7dc20d
Ehab Gamal <ehab.g.sh@gmail.com>
2018-04-25 18:23:27 +0200

    Use connected IntlProvider and addLocaleData

--- examples/redux/src/index.js
+++ examples/redux/src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { addLocaleData } from 'react-intl';
 
+import en from 'react-intl/locale-data/en';
+import ar from 'react-intl/locale-data/ar';
+import fr from 'react-intl/locale-data/fr';
+
+import IntlProvider from './components/intlProvider';
 import App from './components/app';
 import reducers from './reducers';
+
+addLocaleData([...en, ...ar, ...fr]);
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <IntlProvider>
+      <App />
+    </IntlProvider>
   </Provider>
   , document.getElementById('root'));
