411e52741abebc2f89d72aba5f0fe3a9154fcbc0
Ehab Gamal <ehab.g.sh@gmail.com>
2018-04-25 19:50:06 +0200

    InjectIntl props and use intl formats in app component

--- examples/redux/src/components/app.jsx
+++ examples/redux/src/components/app.jsx
@@ -1,24 +1,95 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {
+  FormattedMessage,
+  FormattedHTMLMessage,
+  FormattedNumber,
+  FormattedDate,
+  FormattedTime,
+  FormattedRelative,
+  injectIntl,
+} from 'react-intl';
 
-const App = () => (
-  <div className="container" style={{ paddingTop: '50px' }}>
+import { updateLocales } from '../actionCreators/localesActionCreators';
+import { LOCALES_LANGS } from '../locales/utils';
+
+const App = ({ intl, locale, changeLocale }) => (
+  <div
+    className="container"
+    style={
+      locale === 'ar' 
+      ? { paddingTop: '50px', direction: 'rtl', textAlign: 'right' }
+      : { paddingTop: '50px' }
+    }>
     <div className="jumbotron">
-      <h1 className="display-4">Hello in English!</h1>
+      <h1 className="display-4">
+        <FormattedMessage id="home.top.title" />
+      </h1>
       <div className="form-group row">
-        <label htmlFor="localesList" className="col-sm-12 col-md-2 col-form-label">Change language</label>
-        <select className="form-control col-sm-12 col-md-2" id="localesList">
-          <option>English</option>
-          <option>Frensh</option>
-          <option>Arabic</option>
+        <label htmlFor="localesList" className="col-sm-12 col-md-2 col-form-label">
+          <FormattedMessage id="home.top.locales-list" />
+        </label>
+        <select
+          id="localesList"
+          className="form-control col-sm-12 col-md-2"
+          value={locale}
+          onChange={e => changeLocale(e.target.value)}
+          >
+          {Object.keys(LOCALES_LANGS).map(lang => <option key={lang} value={lang}>{LOCALES_LANGS[lang]}</option>)}
         </select>
       </div>
-      <h4>This is a sample text with <strong>HTML</strong> content.</h4>
+      <h4>
+        <FormattedHTMLMessage id="home.top.html-content" />
+      </h4>
       <hr className="my-2" />
-      <h5>Today is {new Date().toString()}</h5>
-      <h5>This example was developed {new Date().toString()} ago.</h5>
-      <h5>Example of formatted number like 12.34 or price like 24.5$</h5>
+      <h5>
+        <FormattedMessage id="home.bottom.today" />&nbsp;
+        <FormattedDate
+          value={new Date()}
+          year="numeric"
+          month="long"
+          day="numeric"
+        />&nbsp;
+        <FormattedTime value={new Date()}/>
+      </h5>
+      <h5>
+        <FormattedMessage id="home.bottom.pub-date" />&nbsp;
+        <FormattedRelative value={new Date(1524674153739)} />
+      </h5>
+      <h5>
+        <FormattedMessage id="home.bottom.load-time" />&nbsp;
+        <FormattedRelative
+          value={new Date()}
+          updateInterval={1000}
+        />
+      </h5>
+      <h5>
+        <FormattedMessage
+          id="home.bottom.num"
+          values={{num: '12.34'}}
+        />
+        <FormattedMessage
+          id="home.bottom.price"
+          values={{
+            price: intl.formatNumber(
+              '24.5',
+              {
+                style: 'currency',
+                currencyDisplay: 'symbol',
+                currency: locale === 'fr' ? 'EUR' : locale === 'ar' ? 'EGP' : 'USD'
+              }
+            )
+          }}
+        />
+      </h5>
     </div>
   </div>
 );
 
-export default App;
+const mapStateToProps = ({ locales: { locale } }) => ({ locale });
+
+const mapDispatchToProps = dispatch => ({
+  changeLocale: locale => dispatch(updateLocales(locale))
+});
+
+export default injectIntl(connect(mapStateToProps, mapDispatchToProps)(App));
