21fdc81a64f231b65bd758ec4d6e29fc294c3a05
DragonRaider5 <marces.engel@googlemail.com>
2018-06-30 21:18:39 +0200

    Removed name from component returned by withIntlContext() HOC

--- src/components/withIntlContext.js
+++ src/components/withIntlContext.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Consumer as IntlConsumer} from './provider.js';
 
 export default function withIntlContext(Component) {
-  return function WithIntlContext(props) {
+  return function (props) {
     return (
       <IntlConsumer>
         {(intl) => (
