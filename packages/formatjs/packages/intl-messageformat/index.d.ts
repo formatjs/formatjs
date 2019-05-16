d0c8cfab0a33046387302be09a85476542284f23
Cyril Neruchev <menandr@list.ru>
2019-05-16 03:39:18 +0300

    Add types for static method `__addLocaleData` and field `__localeData__` (#179)

--- packages/formatjs/packages/intl-messageformat/index.d.ts
+++ packages/formatjs/packages/intl-messageformat/index.d.ts
@@ -1,7 +1,15 @@
+type LocaleData = {
+    locale: string;
+    [key: string]: any;
+};
+
 declare class IntlMessageFormat {
     constructor(message: string, locales: string | string[], formats?: any);
     format(context?: any): string;
+
     static defaultLocale: string;
+    static __localeData__: LocaleData;
+    static __addLocaleData: (localeData: LocaleData | LocaleData[]) => void;
 }
 
 export default IntlMessageFormat;
