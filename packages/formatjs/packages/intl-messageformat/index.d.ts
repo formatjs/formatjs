eea38d904c6aff5d592515eace3e72884d5c6aed
Raj Nigam <rajington@users.noreply.github.com>
2019-05-15 20:39:47 -0400

    Make `locales` argument optional (#178)

--- packages/formatjs/packages/intl-messageformat/index.d.ts
+++ packages/formatjs/packages/intl-messageformat/index.d.ts
@@ -4,7 +4,7 @@ type LocaleData = {
 };
 
 declare class IntlMessageFormat {
-    constructor(message: string, locales: string | string[], formats?: any);
+    constructor(message: string, locales?: string | string[], formats?: any);
     format(context?: any): string;
 
     static defaultLocale: string;
