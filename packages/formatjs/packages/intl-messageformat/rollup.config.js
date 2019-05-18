3f3f668c89f071823feb60b9965b1a1f9f8c3ff3
longlho <holevietlong@gmail.com>
2019-05-18 14:17:58 -0400

    rerun prettier

--- packages/formatjs/packages/intl-messageformat/rollup.config.js
+++ packages/formatjs/packages/intl-messageformat/rollup.config.js
@@ -1,45 +1,42 @@
 import { uglify } from "rollup-plugin-uglify";
 
-
-export default [{
-  input: './lib/index.js',
-  output: {
-    sourcemap: true,
-      file: 'dist/intl-messageformat.js',
-      format: 'umd',
-      name: 'IntlMessageFormat'
-  },
-},
-{
-    input: './lib/locales.js',
+export default [
+  {
+    input: "./lib/index.js",
     output: {
       sourcemap: true,
-        file: 'dist/intl-messageformat-with-locales.js',
-        format: 'umd',
-        name: 'IntlMessageFormat'
-    },
+      file: "dist/intl-messageformat.js",
+      format: "umd",
+      name: "IntlMessageFormat"
+    }
   },
   {
-    input: './lib/index.js',
+    input: "./lib/locales.js",
     output: {
       sourcemap: true,
-        file: 'dist/intl-messageformat.min.js',
-        format: 'umd',
-        name: 'IntlMessageFormat'
-    },
-    plugins: [
-      uglify()
-    ]
+      file: "dist/intl-messageformat-with-locales.js",
+      format: "umd",
+      name: "IntlMessageFormat"
+    }
   },
   {
-    input: './lib/locales.js',
+    input: "./lib/index.js",
     output: {
       sourcemap: true,
-        file: 'dist/intl-messageformat-with-locales.min.js',
-        format: 'umd',
-        name: 'IntlMessageFormat'
+      file: "dist/intl-messageformat.min.js",
+      format: "umd",
+      name: "IntlMessageFormat"
     },
-    plugins: [
-      uglify()
-    ]
-  }]
+    plugins: [uglify()]
+  },
+  {
+    input: "./lib/locales.js",
+    output: {
+      sourcemap: true,
+      file: "dist/intl-messageformat-with-locales.min.js",
+      format: "umd",
+      name: "IntlMessageFormat"
+    },
+    plugins: [uglify()]
+  }
+];
