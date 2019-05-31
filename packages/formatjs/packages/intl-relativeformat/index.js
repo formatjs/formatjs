f762769206d9daed19c7386bef2a31d45127f4fa
Long Ho <longho@dropbox.com>
2019-05-31 16:23:13 -0400

    bug(intl-relativeformat): expose es6 module entry point

--- packages/formatjs/packages/intl-relativeformat/index.js
+++ packages/formatjs/packages/intl-relativeformat/index.js
@@ -2,11 +2,7 @@
 
 'use strict';
 
-var IntlRelativeFormat = require('./lib/main')['default'];
-
-// Add all locale data to `IntlRelativeFormat`. This module will be ignored when
-// bundling for the browser with Browserify/Webpack.
-require('./lib/locales');
+var IntlRelativeFormat = require('./dist/intl-relativeformat-with-locales');
 
 // Re-export `IntlRelativeFormat` as the CommonJS default exports with all the
 // locale data registered, and with English set as the default locale. Define
