7b6ffa41c5db03438874a2904a06afd0a85bcdb3
Long Ho <holevietlong@gmail.com>
2019-06-08 23:56:45 -0400

    tryin to fix build, attemp 1

--- test/functional/index.js
+++ test/functional/index.js
@@ -3,8 +3,8 @@ import buildTests from './support/build';
 import formatTests from './support/format';
 
 const builds = {
-    'ES'      : p.resolve('lib/index.js'),
-    'CJS'     : p.resolve('dist/index.js'),
+    'ES'      : p.resolve(require('../../package.json').module),
+    'CJS'     : p.resolve(require('../../package.json').main),
     'UMD-dev' : p.resolve('dist/react-intl.js'),
     'UMD-prod': p.resolve('dist/react-intl.min.js'),
 };
