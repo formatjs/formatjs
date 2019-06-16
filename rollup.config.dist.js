623c6cb24ac32173b7aa05d16c8b20cdfcc791c7
Long Ho <holevietlong@gmail.com>
2019-06-15 23:07:36 -0400

    fix format

--- rollup.config.dist.js
+++ rollup.config.dist.js
@@ -43,7 +43,7 @@ export default {
       include: ['*.js+(|x)', '**/*.js+(|x)'],
     }),
     commonjs({
-      sourcemap: true
+      sourcemap: true,
     }),
     isProduction &&
       uglify({
