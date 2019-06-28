dd884a540070877818146a2b123ea7d1ae2cf6ec
Long Ho <holevietlong@gmail.com>
2019-06-28 10:57:33 -0400

    rm unused test utils

--- test/unit/testUtils.tsx
+++ test/unit/testUtils.tsx
@@ -6,35 +6,7 @@ import {
   DEFAULT_INTL_CONFIG,
 } from '../../src/utils';
 import {WithIntlProps} from '../../src/components/injectIntl';
-import Provider, {
-  Props as ProviderProps,
-  OptionalIntlConfig,
-  getBoundFormatFns,
-} from '../../src/components/provider';
-import {IntlShape} from '../../src/types';
-export const shallowDeep = (componentInstance, depth, options) => {
-  let rendered = shallow(componentInstance, options);
-
-  for (let i = 1; i < depth; i++) {
-    rendered = rendered.dive();
-  }
-
-  return rendered;
-};
-
-export class SpyComponent extends React.Component {
-  private _renders: number = 0;
-
-  getRenderCount() {
-    return this._renders;
-  }
-
-  render() {
-    this._renders++;
-
-    return null;
-  }
-}
+import Provider, {Props as ProviderProps} from '../../src/components/provider';
 
 export function mountFormattedComponentWithProvider<P>(
   Comp: React.ComponentType<P>
