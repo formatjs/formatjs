87058015b286db3732f3750f61c2d324243235e0
Long Ho <holevietlong@gmail.com>
2019-07-06 21:36:45 -0400

    prettier

--- src/components/createFormattedComponent.tsx
+++ src/components/createFormattedComponent.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from 'react';
 import {IntlShape, FormatDateOptions, FormatNumberOptions} from '../types';
 import withIntl from './injectIntl';
 
--- src/components/date.tsx
+++ src/components/date.tsx
@@ -3,11 +3,11 @@
  * Copyrights licensed under the New BSD License.
  * See the accompanying LICENSE file for terms.
  */
-import createFormattedComponent from './createFormattedComponent'
+import createFormattedComponent from './createFormattedComponent';
 
-const {
-  BaseComponent: _BaseComponent, Component
-} = createFormattedComponent('formatDate')
+const {BaseComponent: _BaseComponent, Component} = createFormattedComponent(
+  'formatDate'
+);
 
-export const BaseComponent = _BaseComponent
-export default Component
+export const BaseComponent = _BaseComponent;
+export default Component;
