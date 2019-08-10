003d5b339770c07dc274c90d0d137855d4f40deb
Paweł Dąbrowski <pdabrowski@infront.com.pl>
2019-08-11 00:32:35 +0200

    moved umd build config to separate file, removed concatenated cjs build

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import * as p from 'path';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
-import {terser} from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 const copyright = `/*
@@ -25,42 +24,14 @@ const plugins = [
   typescript({
     rollupCommonJSResolveHack: true,
   }),
-  terser({
-    include: [/^.+\.min\.js$/],
-  }),
 ].filter(Boolean);
-
-const umdBuildOutput = {
-  format: 'umd',
-  name: 'ReactIntl',
-  banner: copyright,
-  exports: 'named',
-  sourcemap: true,
-  globals: {
-    react: 'React',
-  },
-};
 
 export default [
   {
-    input: 'src/index.ts',
+    input: p.resolve('src/index.ts'),
     output: [
       {
-        file: p.resolve(`dist/react-intl.min.js`),
-        ...umdBuildOutput,
-      },
-      {
-        file: p.resolve(`dist/react-intl.js`),
-        ...umdBuildOutput,
-      },
-      {
-        file: 'dist/index.js',
-        format: 'cjs',
-        banner: copyright,
-        sourcemap: true,
-      },
-      {
-        file: 'dist/index.es.js',
+        file: p.resolve('dist/index.es.js'),
         format: 'es',
         banner: copyright,
         sourcemap: true,
---
+++ rollup.config.umd.js
@@ -0,0 +1,65 @@
+import * as p from 'path';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import {uglify} from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const copyright = `/*
+ * Copyright ${new Date().getFullYear()}, Yahoo Inc.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+`;
+
+const plugins = [
+  replace({
+    replaces: {
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    },
+  }),
+  nodeResolve(),
+  babel({
+    configFile: false,
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            ie: '11',
+            esmodules: true,
+          },
+          modules: false,
+        },
+      ],
+    ],
+  }),
+  commonjs({
+    sourcemap: true,
+  }),
+  isProduction &&
+    uglify({
+      warnings: false,
+    }),
+].filter(Boolean);
+
+export default [
+  {
+    input: p.resolve('dist/index.es.js'),
+    output: {
+      file: p.resolve(`dist/react-intl.${isProduction ? 'min.js' : 'js'}`),
+      format: 'umd',
+      name: 'ReactIntl',
+      banner: copyright,
+      exports: 'named',
+      sourcemap: true,
+      globals: {
+        react: 'React',
+      },
+    },
+    external: ['react'],
+    plugins,
+  },
+];
