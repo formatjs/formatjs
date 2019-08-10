b510bb74945194900c908e28d30612752be84401
Long Ho <longho@dropbox.com>
2019-08-09 20:43:45 -0400

    chore(@formatjs/intl-relativetimeformat): fix test for node12

--- packages/formatjs/packages/intl-relativetimeformat/tests/index.test.ts
+++ packages/formatjs/packages/intl-relativetimeformat/tests/index.test.ts
@@ -4,10 +4,10 @@ import '../src/polyfill-locales';
 describe('Intl.RelativeTimeFormat', function() {
   it('should support aliases', function() {
     expect(
-      new Intl.RelativeTimeFormat('zh-CN', undefined).resolvedOptions().locale
-    ).toBe('zh-Hans');
+      new Intl.RelativeTimeFormat('zh-CN', undefined).format(-1, 'second')
+    ).toBe('1秒钟前');
     expect(
-      new Intl.RelativeTimeFormat('zh-TW', undefined).resolvedOptions().locale
-    ).toBe('zh-Hant');
+      new Intl.RelativeTimeFormat('zh-TW', undefined).format(-1, 'second')
+    ).toBe('1 秒前');
   });
 });
