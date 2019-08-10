889f79ce13c27b796cd8b2a58c989508bfb8b81d
Paweł Dąbrowski <pdabrowski@infront.com.pl>
2019-08-10 23:01:48 +0200

    changed rollup config to make it work on create-react-app

--- rollup.config.dist.js
+++ rollup.config.dist.js
@@ -2,10 +2,8 @@ import * as p from 'path';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
-import {uglify} from 'rollup-plugin-uglify';
-import babel from 'rollup-plugin-babel';
-
-const isProduction = process.env.NODE_ENV === 'production';
+import {terser} from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 const copyright = `/*
  * Copyright ${new Date().getFullYear()}, Yahoo Inc.
@@ -21,44 +19,53 @@ const plugins = [
     },
   }),
   nodeResolve(),
-  babel({
-    configFile: false,
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            ie: '11',
-            esmodules: true,
-          },
-          modules: false,
-        },
-      ],
-    ],
-  }),
   commonjs({
     sourcemap: true,
   }),
-  isProduction &&
-    uglify({
-      warnings: false,
-    }),
+  typescript({
+    rollupCommonJSResolveHack: true,
+  }),
+  terser({
+    include: [/^.+\.min\.js$/],
+  }),
 ].filter(Boolean);
+
+const umdBuildOutput = {
+  format: 'umd',
+  name: 'ReactIntl',
+  banner: copyright,
+  exports: 'named',
+  sourcemap: true,
+  globals: {
+    react: 'React',
+  },
+};
 
 export default [
   {
-    input: p.resolve('dist/index.mjs'),
-    output: {
-      file: p.resolve(`dist/react-intl.${isProduction ? 'min.js' : 'js'}`),
-      format: 'umd',
-      name: 'ReactIntl',
-      banner: copyright,
-      exports: 'named',
-      sourcemap: true,
-      globals: {
-        react: 'React',
+    input: 'src/index.ts',
+    output: [
+      {
+        file: p.resolve(`dist/react-intl.min.js`),
+        ...umdBuildOutput,
       },
-    },
+      {
+        file: p.resolve(`dist/react-intl.js`),
+        ...umdBuildOutput,
+      },
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        banner: copyright,
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+        banner: copyright,
+        sourcemap: true,
+      },
+    ],
     external: ['react'],
     plugins,
   },
