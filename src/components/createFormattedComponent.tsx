c931d84c4681de3a7a5b80c62f8439971a5b34a3
Nicola Molinari <nicola.molinari@commercetools.de>
2019-08-13 08:06:25 +0200

    refactor: don't use HOC for formatted component factory

--- src/components/createFormattedComponent.tsx
+++ src/components/createFormattedComponent.tsx
@@ -1,46 +1,54 @@
 import * as React from 'react';
+import {invariantIntlContext} from '../utils';
 import {IntlShape, FormatDateOptions, FormatNumberOptions} from '../types';
-import withIntl, {WrappedComponentProps} from './injectIntl';
+import {Context} from './injectIntl';
 
-export default function createFormattedComponent<
-  T extends 'formatDate' | 'formatTime' | 'formatNumber'
->(type: T) {
-  type FormatOptions<T> = T extends 'formatDate'
-    ? FormatDateOptions
-    : T extends 'formatTime'
-    ? FormatDateOptions
-    : FormatNumberOptions;
+type FormatFunctionName = 'formatDate' | 'formatTime' | 'formatNumber';
+type FormatOptions<T> = T extends 'formatDate'
+  ? FormatDateOptions
+  : T extends 'formatTime'
+  ? FormatDateOptions
+  : FormatNumberOptions;
+
+const displayNames: {
+  formatDate: 'FormattedDate';
+  formatTime: 'FormattedTime';
+  formatNumber: 'FormattedNumber';
+} = {
+  formatDate: 'FormattedDate',
+  formatTime: 'FormattedTime',
+  formatNumber: 'FormattedNumber',
+};
+
+export default function createFormattedComponent<T extends FormatFunctionName>(
+  type: T
+) {
   type FormatFn = IntlShape[T];
-  type Props = FormatOptions<T> &
-    WrappedComponentProps & {
-      value: Parameters<FormatFn>[0];
-      children?(val: string): React.ReactElement | null;
-    };
-  const Component: React.FC<Props> = props => {
-    const {
-      value,
-      children,
-      intl: {[type]: formatFn, textComponent: Text},
-    } = props;
-
-    let formattedValue = formatFn(value as any, props);
-
-    if (typeof children === 'function') {
-      return children(formattedValue);
-    }
-    if (Text) {
-      return <Text>{formattedValue}</Text>;
-    }
-    // Work around @types/react where React.FC cannot return string
-    return formattedValue as any;
+  type Props = FormatOptions<T> & {
+    value: Parameters<FormatFn>[0];
+    children?: (val: string) => React.ReactElement | null;
   };
-  Component.displayName =
-    type === 'formatDate'
-      ? 'FormattedDate'
-      : type === 'formatTime'
-      ? 'FormattedTime'
-      : 'FormattedNumber';
-  return {
-    Component: withIntl(Component),
-  };
+
+  const Component = (props: Props) => (
+    <Context.Consumer>
+      {intl => {
+        invariantIntlContext(intl);
+
+        const {value, children} = props;
+        const {[type]: formatFn, textComponent: Text} = intl;
+        const formattedValue = formatFn(value as any, props);
+
+        if (typeof children === 'function') {
+          return children(formattedValue);
+        }
+        if (Text) {
+          return <Text>{formattedValue}</Text>;
+        }
+        // Work around @types/react where React.FC cannot return string
+        return <>{formattedValue as any}</>;
+      }}
+    </Context.Consumer>
+  );
+  Component.displayName = displayNames[type];
+  return Component;
 }
--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,9 @@ export {
 } from './components/injectIntl';
 export {default as useIntl} from './components/useIntl';
 export {default as IntlProvider, createIntl} from './components/provider';
-export const {Component: FormattedDate} = createFormattedComponent(
-  'formatDate'
-);
-export const {Component: FormattedTime} = createFormattedComponent(
-  'formatTime'
-);
-export const {Component: FormattedNumber} = createFormattedComponent(
-  'formatNumber'
-);
+export const FormattedDate = createFormattedComponent('formatDate');
+export const FormattedTime = createFormattedComponent('formatTime');
+export const FormattedNumber = createFormattedComponent('formatNumber');
 export {default as FormattedRelativeTime} from './components/relative';
 export {default as FormattedPlural} from './components/plural';
 export {default as FormattedMessage} from './components/message';
