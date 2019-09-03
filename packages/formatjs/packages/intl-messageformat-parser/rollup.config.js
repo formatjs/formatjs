c0d4489490408692f812daf3a7083fd25a00e06b
Long Ho <holevietlong@gmail.com>
2019-09-02 22:43:01 -0400

    feat(intl-messageformat-parser): add UMD dist, fixes #171

---
+++ packages/formatjs/packages/intl-messageformat-parser/rollup.config.js
@@ -0,0 +1,31 @@
+import { uglify } from 'rollup-plugin-uglify';
+import resolve from 'rollup-plugin-node-resolve';
+
+const resolveConfig = resolve({
+  mainFields: ['module', 'main']
+});
+const uglifyConfig = uglify();
+export default [
+  {
+    input: './lib/index.js',
+    output: {
+      sourcemap: true,
+      file: 'dist/umd/intl-messageformat-parser.js',
+      format: 'umd',
+      exports: 'named',
+      name: 'IntlMessageFormatParser'
+    },
+    plugins: [resolveConfig]
+  },
+  {
+    input: './lib/index.js',
+    output: {
+      sourcemap: true,
+      file: 'dist/umd/intl-messageformat-parser.min.js',
+      format: 'umd',
+      exports: 'named',
+      name: 'IntlMessageFormatParser'
+    },
+    plugins: [resolveConfig, uglifyConfig]
+  }
+];
