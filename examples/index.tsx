cd3254018e241d4330f781dd13c3c9f132edf076
Kipras Melnikovas <kipras@kipras.org>
2019-09-08 19:50:58 +0300

    Use `examples/Hooks.tsx` in `examples/index.(tsx|html)`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- examples/index.tsx
+++ examples/index.tsx
@@ -5,12 +5,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Timezone from './TimeZone';
 import Messages from './Messages';
+import Hooks from './Hooks';
 import Advanced from './Advanced';
 import Injected from './Injected';
 
 ReactDOM.render(<Timezone />, document.getElementById('timezone'));
 
 ReactDOM.render(<Messages />, document.getElementById('messages'));
+ReactDOM.render(<Hooks />, document.getElementById('hooks'));
 
 ReactDOM.render(<Advanced />, document.getElementById('advanced'));
 ReactDOM.render(<Injected />, document.getElementById('injected'));
