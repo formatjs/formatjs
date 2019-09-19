5a3c2822477e5b325dc2b5b6ff3d3a828dd8157e
Long Ho <holevietlong@gmail.com>
2019-09-19 19:35:07 -0400

    feat(@formatjs/intl-unified-numberformat): mark the package as side-effects free BREAKING CHANGE: This package no longer comes with `en` by default

--- packages/formatjs/packages/intl-unified-numberformat/src/polyfill.ts
+++ packages/formatjs/packages/intl-unified-numberformat/src/polyfill.ts
@@ -1,7 +1,4 @@
 import {UnifiedNumberFormat, isUnitSupported} from './core';
-import en from './en';
-
-UnifiedNumberFormat.__addUnitLocaleData(en);
 
 if (!isUnitSupported('bit')) {
   Intl.NumberFormat = UnifiedNumberFormat as any;
