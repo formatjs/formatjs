b9ae722c6f90b2be0d77e6c6ff7efb69c0163e43
Long Ho <holevietlong@gmail.com>
2019-10-29 21:38:09 -0400

    chore(@formatjs/macro): Add macro package

---
+++ packages/formatjs/packages/macro/src/index.ts
@@ -0,0 +1,9 @@
+export function defineMessage<T>(msg: T): T {
+    return msg
+}
+
+export function defineMessages<T>(msgs: Record<string, T>): Record<string, T> {
+    return msgs
+}
+
+export const _ = defineMessage
