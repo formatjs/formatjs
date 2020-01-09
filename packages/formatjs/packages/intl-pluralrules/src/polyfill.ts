5f7337fb7acd9d3dea0a5f13a6fcce9dbd0bd5e4
Long Ho <holevietlong@gmail.com>
2020-01-09 00:14:02 -0500

    feat(@formatjs/intl-pluralrules): also polyfill env where it doesn’t take in minimumFractionDigits

--- packages/formatjs/packages/intl-pluralrules/src/polyfill.ts
+++ packages/formatjs/packages/intl-pluralrules/src/polyfill.ts
@@ -1,6 +1,10 @@
 import {PluralRules} from './core';
 
-if (typeof Intl.PluralRules === 'undefined') {
+if (
+  typeof Intl.PluralRules === 'undefined' ||
+  new Intl.PluralRules('en', {minimumFractionDigits: 2} as any).select(1) ===
+    'one'
+) {
   Object.defineProperty(Intl, 'PluralRules', {
     value: PluralRules,
     writable: true,
