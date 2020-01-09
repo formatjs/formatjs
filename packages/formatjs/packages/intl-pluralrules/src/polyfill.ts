948afed47ada9e00c0b35fef3ef330b6a32a43a1
Long Ho <holevietlong@gmail.com>
2020-01-09 00:31:22 -0500

    fix(@formatjs/intl-pluralrules): tweak polyfill checks

--- packages/formatjs/packages/intl-pluralrules/src/polyfill.ts
+++ packages/formatjs/packages/intl-pluralrules/src/polyfill.ts
@@ -2,8 +2,9 @@ import {PluralRules} from './core';
 
 if (
   typeof Intl.PluralRules === 'undefined' ||
-  new Intl.PluralRules('en', {minimumFractionDigits: 2} as any).select(1) ===
-    'one'
+  (!(Intl.PluralRules as any).polyfilled &&
+    new Intl.PluralRules('en', {minimumFractionDigits: 2} as any).select(1) ===
+      'one')
 ) {
   Object.defineProperty(Intl, 'PluralRules', {
     value: PluralRules,
