146ca31447a8790a9799eb91751f811f8d1aca08
Axell Brendow Batista Moreira <breno.axel@gmail.com>
2020-03-28 16:38:20 -0300

    chore: add an example (formatjs#1441) (#1610)
    This example shows how to change the language and the messages when
    using the createIntl() function. It also allows to use
    intl object in other files even after a language change.

---
+++ examples/HandleChange.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import {createIntl, createIntlCache, RawIntlProvider} from '../';
+
+const messages: Record<string, object> = {
+  'en-US': {selectalanguage: 'Select a language'},
+  'pt-BR': {selectalanguage: 'Selecione uma linguagem'},
+};
+
+const initialLocale = 'en-US';
+export const cache = createIntlCache();
+/** You can use this variable in other files even after reassigning it. */
+export let intl = createIntl(
+  {locale: initialLocale, messages: messages[initialLocale]},
+  cache
+);
+export let fmt = intl.formatMessage;
+
+const App: React.FC = () => {
+  // You could use redux to get the locale or get it from the url.
+  const [locale, setLocale] = React.useState(initialLocale);
+
+  const changeLanguage = (newLocale: string) => {
+    intl = createIntl(
+      {locale: newLocale, messages: messages[newLocale]},
+      cache
+    );
+    fmt = intl.formatMessage;
+    document.documentElement.lang = newLocale;
+    setLocale(newLocale);
+  };
+
+  return (
+    <RawIntlProvider value={intl}>
+      <h1>{fmt({id: 'selectalanguage'})}</h1>
+      <select
+        name="locale"
+        defaultValue={locale}
+        id="locale"
+        onChange={event => changeLanguage(event.target.value)}
+      >
+        {Object.keys(messages).map(locale => (
+          <option value={locale}>{locale}</option>
+        ))}
+      </select>
+    </RawIntlProvider>
+  );
+};
+
+export default App;
