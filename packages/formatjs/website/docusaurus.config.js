b32e924d2386e4e1022ba383c7ec8d348faabbf3
Long Ho <longho@dropbox.com>
2020-03-29 23:55:40 -0400

    chore: tweak config

--- packages/formatjs/website/docusaurus.config.js
+++ packages/formatjs/website/docusaurus.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: 'Format.JS',
   tagline: 'Internationalize your web apps on the client & server.',
-  url: 'https://formatjs.io/',
-  baseUrl: '/',
+  url: 'https://formatjs.github.io/',
+  baseUrl: '/formatjs/',
   favicon: 'img/favicon.ico',
   organizationName: 'formatjs', // Usually your GitHub org/user name.
   projectName: 'formatjs', // Usually your repo name.
