0100beb5f99691a5dfe6145cd8bca2fa72c5693b
Long Ho <longho@dropbox.com>
2020-04-27 15:54:46 -0400

    fix(docs): fix Get Started URL

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -262,7 +262,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted
               )}
-              to={useBaseUrl('docs/doc1')}
+              to={useBaseUrl('docs/basic-internationalization-principles')}
             >
               <FormattedMessage
                 defaultMessage="Get Started"
