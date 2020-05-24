555a28b2d6d3c48f69d765c5090f7880ef603adc
Long Ho <holevietlong@gmail.com>
2020-05-23 21:32:46 -0400

    fix(@formatjs/intl-getcanonicallocales): set the polyfill if native impl is buggy

--- packages/intl-getcanonicallocales/src/polyfill.ts
+++ packages/intl-getcanonicallocales/src/polyfill.ts
@@ -1,5 +1,9 @@
 import {getCanonicalLocales} from './';
-if (!('getCanonicalLocales' in Intl)) {
+if (
+  !('getCanonicalLocales' in Intl) ||
+  // Native Intl.getCanonicalLocales is just buggy
+  (Intl as any).getCanonicalLocales('und-x-private') === 'x-private'
+) {
   Object.defineProperty(Intl, 'getCanonicalLocales', {
     value: getCanonicalLocales,
     writable: true,
--- packages/intl-getcanonicallocales/tests/index.test.ts
+++ packages/intl-getcanonicallocales/tests/index.test.ts
@@ -1,10 +1,13 @@
 import {getCanonicalLocales} from '../src';
 
 describe('Intl.getCanonicalLocales', () => {
-  it('toString', function () {
+  it('regular', function () {
     expect(
       getCanonicalLocales('en-u-foo-bar-nu-thai-ca-buddhist-kk-true')
     ).toEqual(['en-u-bar-foo-ca-buddhist-kk-nu-thai']);
+  });
+  it('und-x-private', function () {
+    expect(getCanonicalLocales('und-x-private')).toEqual(['und-x-private']);
   });
   // it('canonicalizes twice', function () {
   //   expect(getCanonicalLocales('und-Armn-SU')).toEqual(['ru-Armn-AM']);
