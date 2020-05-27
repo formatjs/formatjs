79bcf897b3e29c5a5f4edc5946fbecea2feb3b8b
Long Ho <holevietlong@gmail.com>
2020-05-27 08:49:23 -0400

    chore(react-intl): fix tests for Node 12

--- packages/react-intl/test/setup.js
+++ packages/react-intl/test/setup.js
@@ -1,4 +1,9 @@
 const {configure} = require('enzyme');
+if (process.version.startsWith('v12')) {
+  delete Intl.PluralRules;
+  delete Intl.RelativeTimeFormat;
+  delete Intl.NumberFormat;
+}
 require('@formatjs/intl-pluralrules/polyfill-locales');
 require('@formatjs/intl-relativetimeformat/polyfill-locales');
 require('@formatjs/intl-listformat/polyfill-locales');
