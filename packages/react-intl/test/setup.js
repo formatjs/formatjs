9f43b8151ffd25904cbccaa4e00e776ee75ddaf4
Long Ho <holevietlong@gmail.com>
2020-05-27 00:14:59 -0400

    chore: rm full-icu dep

--- packages/react-intl/test/setup.js
+++ packages/react-intl/test/setup.js
@@ -3,6 +3,9 @@ require('@formatjs/intl-pluralrules/polyfill-locales');
 require('@formatjs/intl-relativetimeformat/polyfill-locales');
 require('@formatjs/intl-listformat/polyfill-locales');
 require('@formatjs/intl-displaynames/polyfill-locales');
+require('@formatjs/intl-numberformat/polyfill');
+require('@formatjs/intl-numberformat/dist/locale-data/en');
+require('@formatjs/intl-numberformat/dist/locale-data/es');
 const Adapter = require('enzyme-adapter-react-16');
 
 configure({adapter: new Adapter()});
