2b549160f55898bf3e625a9c8e8e82c4ec4fc47e
Long Ho <longho@dropbox.com>
2020-07-02 18:04:50 -0400

    docs(@formatjs/intl-displaynames): fix comment

--- packages/intl-displaynames/scripts/cldr.ts
+++ packages/intl-displaynames/scripts/cldr.ts
@@ -26,7 +26,7 @@ function main(args: minimist.ParsedArgs) {
   const {outDir, testDataDir, polyfillLocalesOutFile, test262MainFile} = args;
 
   if (outDir) {
-    // Dist all locale files to dist/locale-data (JS)
+    // Dist all locale files to locale-data (JS)
     Object.keys(allData).forEach(function (lang) {
       const destFile = join(outDir, lang + '.js');
       outputFileSync(
