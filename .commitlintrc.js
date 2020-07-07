39d2616d6f7b9aa8d7d02a540356cbe70180eacf
Long Ho <holevietlong@gmail.com>
2020-07-07 00:08:43 -0400

    Revert "feat: fix commitlint, add postinstall-postinstall, bump bazel version, add color output to Jest"
    This reverts commit 0e401bf5b0434dbebf19aff8591ebf4b2e496833.

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -6,10 +6,5 @@ const packages = globSync('./packages/*/package.json').map(
 
 module.exports = {
   extends: ['@commitlint/config-angular'],
-  rules: {
-    // Cheatsheet: https://commitlint.js.org/#/reference-rules
-    // Sweet Jesus why is disabling a rule syntax so verbose??
-    'scope-enum': [2, 'always', packages],
-    'header-max-length': [0, 'never', Infinity],
-  },
+  'scope-enum': packages,
 };
