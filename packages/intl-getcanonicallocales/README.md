507adf2e229ddc86fa960e07297ba2953be7dc5f
Long Ho <holevietlong@gmail.com>
2020-07-07 01:04:39 -0400

    build(@formatjs/intl-getcanonicallocales): fix up BUILD file

--- packages/intl-getcanonicallocales/README.md
+++ packages/intl-getcanonicallocales/README.md
@@ -1,3 +1,3 @@
 # Intl.getCanonicalLocales
 
-We've migrated the docs to https://formatjs.io.
+We've migrated the docs to https://formatjs.io/docs/polyfills/intl-getcanonicallocales/.
--- packages/intl-getcanonicallocales/polyfill.ts
+++ packages/intl-getcanonicallocales/polyfill.ts
@@ -13,6 +13,7 @@ if (typeof Intl === 'undefined') {
 if (
   !('getCanonicalLocales' in Intl) ||
   // Native Intl.getCanonicalLocales is just buggy
+  // https://bugs.chromium.org/p/v8/issues/detail?id=10682
   ((Intl as any).getCanonicalLocales as typeof getCanonicalLocales)(
     'und-x-private'
   )[0] === 'x-private'
