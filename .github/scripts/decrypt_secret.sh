7971199a787d4a2d399171ddebff3093b2bd6be5
Long Ho <holevietlong@gmail.com>
2020-07-08 21:54:35 -0400

    build: tweak secret

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_BAZEL_CACHE_JSON_PASSPHRASE" \
---output $HOME/secrets/secret.json secret.json.gpg
+--output $GITHUB_WORKSPACE/../secret.json secret.json.gpg
