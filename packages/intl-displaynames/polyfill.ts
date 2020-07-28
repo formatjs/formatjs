c72f00079d1f1c8203e8da05badac498c1378583
Roderick Hsiao <roderickhsiao@users.noreply.github.com>
2020-07-28 07:47:55 -0700

    fix(@formatjs/intl-datetimeformat): handle missing ICU data bug (#1886) fix #1879

--- packages/intl-displaynames/polyfill.ts
+++ packages/intl-displaynames/polyfill.ts
@@ -12,7 +12,18 @@ declare global {
   }
 }
 
-if (!Intl.DisplayNames) {
+/**
+ * https://bugs.chromium.org/p/chromium/issues/detail?id=1097432
+ */
+function hasMissingICUBug() {
+  if (Intl.DisplayNames) {
+    const regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+    return regionNames.of('CA') === 'CA';
+  }
+  return false;
+}
+
+if (!Intl.DisplayNames || hasMissingICUBug()) {
   Object.defineProperty(Intl, 'DisplayNames', {
     value: DisplayNames,
     enumerable: false,
