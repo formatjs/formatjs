d25e520acd8159b946ae05b284e4ce5682d55565
Pablo Damnhorns <pavel.chertorogov@gmail.com>
2020-08-03 23:22:45 +0600

    fix(ts-transformer): allow to override user defined message id via `overrideIdFn` as function

--- packages/ts-transformer/src/transform.ts
+++ packages/ts-transformer/src/transform.ts
@@ -164,16 +164,18 @@ function extractMessageDescriptor(
   if (!msg.defaultMessage && !msg.id) {
     return;
   }
-  if (!msg.id && msg.defaultMessage && overrideIdFn) {
+  if (msg.defaultMessage && overrideIdFn) {
     switch (typeof overrideIdFn) {
       case 'string':
-        msg.id = interpolateName(
-          {sourcePath: sf.fileName} as any,
-          overrideIdFn,
-          {
-            content: msg.description
-              ? `${msg.defaultMessage}#${msg.description}`
-              : msg.defaultMessage,
+        if (!msg.id) {
+          msg.id = interpolateName(
+            {sourcePath: sf.fileName} as any,
+            overrideIdFn,
+            {
+              content: msg.description
+                ? `${msg.defaultMessage}#${msg.description}`
+                : msg.defaultMessage,
+            }
           }
         );
         break;
