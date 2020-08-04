fb86d4f708a4e472e178b33ddf51145b0bf226c2
Daniel Hong <danielhong@soongsil.ac.kr>
2020-08-05 03:33:28 +0900

    fix: add more activeBasePath with Regex

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -26,27 +26,26 @@ module.exports = {
       items: [
         {
           to: 'docs/getting-started/installation',
-          activeBasePath: 'docs',
           label: 'Docs',
           position: 'left',
+          activeBaseRegex: `docs/(getting-started|guides|core-concepts)`,
         },
         {
           to: 'docs/react-intl/components',
-          activeBasePath: 'api',
           label: 'API References',
           position: 'left',
+          activeBaseRegex: `docs/(react-intl|intl-messageformat|intl-messageformat-parser)`,
         },
         {
           to: 'docs/polyfills',
-          activeBasePath: 'polyfills',
           label: 'Polyfills',
           position: 'left',
         },
         {
           to: 'docs/tooling/cli',
-          activeBasePath: 'tooling',
           label: 'Tooling',
           position: 'left',
+          activeBaseRegex: `docs/tooling/(cli|linter|babel-plugin|ts-transformer)`,
         },
         {
           href: 'https://github.com/formatjs/formatjs',
