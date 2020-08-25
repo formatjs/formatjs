89e19d400ab830489d7a050e9a45559750718f3b
Long Ho <holevietlong@gmail.com>
2020-08-25 01:25:24 -0400

    feat(@formatjs/ecma402-abstract): add CanonicalCodeForDisplayNames

---
+++ packages/ecma402-abstract/src/DisplayNames/CanonicalCodeForDisplayNames.ts
@@ -0,0 +1,39 @@
+import {CanonicalizeLocaleList} from '../CanonicalizeLocaleList';
+import {invariant} from '../utils';
+import {IsWellFormedCurrencyCode} from '../IsWellFormedCurrencyCode';
+
+const UNICODE_REGION_SUBTAG_REGEX = /^([a-z]{2}|[0-9]{3})$/i;
+const ALPHA_4 = /^[a-z]{4}$/i;
+function isUnicodeRegionSubtag(region: string): boolean {
+  return UNICODE_REGION_SUBTAG_REGEX.test(region);
+}
+
+function isUnicodeScriptSubtag(script: string): boolean {
+  return ALPHA_4.test(script);
+}
+
+export function CanonicalCodeForDisplayNames(
+  type: 'language' | 'region' | 'script' | 'currency',
+  code: string
+) {
+  if (type === 'language') {
+    return CanonicalizeLocaleList([code])[0];
+  }
+  if (type === 'region') {
+    if (!isUnicodeRegionSubtag(code)) {
+      throw RangeError('invalid region');
+    }
+    return code.toUpperCase();
+  }
+  if (type === 'script') {
+    if (!isUnicodeScriptSubtag(code)) {
+      throw RangeError('invalid script');
+    }
+    return `${code[0].toUpperCase()}${code.slice(1)}`;
+  }
+  invariant(type === 'currency', 'invalid type');
+  if (!IsWellFormedCurrencyCode(code)) {
+    throw RangeError('invalid currency');
+  }
+  return code.toUpperCase();
+}
