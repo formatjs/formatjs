f227a86cd08c37c31816a86dc526d81444a3c264
Long Ho <holevietlong@gmail.com>
2020-09-07 01:36:54 -0400

    build: try re-activating zic

--- packages/intl-datetimeformat/scripts/zic.ts
+++ packages/intl-datetimeformat/scripts/zic.ts
@@ -1,30 +1,12 @@
 import {execFile as _execFile} from 'child_process';
 import {promisify} from 'util';
-import {resolve} from 'path';
 import minimist from 'minimist';
 const execFile = promisify(_execFile);
 
-const ZONE_FILENAMES = [
-  'africa',
-  'antarctica',
-  'asia',
-  'australasia',
-  'etcetera',
-  'europe',
-  'northamerica',
-  'southamerica',
-  'pacificnew',
-  'backward',
-];
-
 async function main(args: minimist.ParsedArgs) {
-  const {dataDir, outDir} = args;
+  const {zoneFiles, outDir} = args;
   try {
-    execFile('/usr/sbin/zic', [
-      '-d',
-      outDir,
-      ...ZONE_FILENAMES.map(fn => resolve(dataDir, fn)),
-    ]);
+    execFile('/usr/sbin/zic', ['-d', outDir, ...zoneFiles.split(',')]);
   } catch (e) {
     console.error(e);
     process.exitCode = 1;
