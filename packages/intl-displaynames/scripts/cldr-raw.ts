654cd8b7711216f710933489a4fa1ee5194b6d1e
Long Ho <holevietlong@gmail.com>
2020-11-19 23:42:29 -0500

    feat(@formatjs/intl-displaynames): enhance best fit locale negotiation
    BREAKING CHANGE: This polyfill now requires Intl.Locale, or
    @formatjs/intl-locale polyfill

--- packages/intl-displaynames/scripts/cldr-raw.ts
+++ packages/intl-displaynames/scripts/cldr-raw.ts
@@ -1,35 +1,23 @@
 import {extractDisplayNames, getAllLocales} from './extract-displaynames';
 import {join} from 'path';
 import {outputJSONSync} from 'fs-extra';
-import {DisplayNamesLocaleData} from '@formatjs/ecma402-abstract';
+
 import minimist from 'minimist';
 
 async function main(args: minimist.ParsedArgs) {
   const {outDir} = args;
-  const locales: string[] = await getAllLocales();
+  const locales = await getAllLocales();
   const data = await extractDisplayNames(locales);
-  const allData = locales.reduce(
-    (all: Record<string, DisplayNamesLocaleData>, locale) => {
-      if (locale === 'en-US-POSIX') {
-        all['en-US'] = {
-          data: data[locale],
-          locale: 'en-US',
-        };
-      } else {
-        all[locale] = {
-          data: data[locale],
-          locale,
-        };
-      }
-
-      return all;
-    },
-    {}
-  );
-
-  Object.keys(allData).forEach(function (locale) {
-    outputJSONSync(join(outDir, `${locale}.json`), allData[locale]);
-  });
+  for (let locale of locales) {
+    const d = data[locale];
+    if (locale === 'en-US-POSIX') {
+      locale = 'en-US';
+    }
+    outputJSONSync(join(outDir, `${locale}.json`), {
+      data: d,
+      locale,
+    });
+  }
 }
 
 if (require.main === module) {
