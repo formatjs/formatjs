f3a71887529467650749da414f6cb1e039557ddd
Long Ho <holevietlong@gmail.com>
2021-01-06 23:34:59 -0500

    fix(@formatjs/intl-pluralrules): fix plural compiler patch

--- packages/intl-pluralrules/tests/locale-data/fr.ts
+++ packages/intl-pluralrules/tests/locale-data/fr.ts
@@ -2,7 +2,7 @@
 // prettier-ignore
 // @ts-nocheck
 export default {"data":{"categories":{"cardinal":["one","many","other"],"ordinal":["one","other"]},"fn":function(n, ord) {
-  const _n = String(n), se = _n.split(/[ce]/), e = se[1] || 0, c = e, s = String(e ? Number(se[0]) * Math.pow(10, e) : _n).split("."), i = s[0], v0 = !s[1], i1000000 = i.slice(-6);
+  var _n = String(n), se = _n.split(/[ce]/), e = se[1] || 0, c = e, s = String(e ? Number(se[0]) * Math.pow(10, e) : _n).split("."), i = s[0], v0 = !s[1], i1000000 = i.slice(-6);
   if (ord) return n == 1 ? 'one' : 'other';
   return n >= 0 && n < 2 ? 'one'
     : e == 0 && i != 0 && i1000000 == 0 && v0 || (e < 0 || e > 5) ? 'many'
