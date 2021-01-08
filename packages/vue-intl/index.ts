d06c4bc7b04ba269e3313c39cf76695fc70aea33
Long Ho <holevietlong@gmail.com>
2021-01-07 21:45:36 -0500

    refactor(vue-intl): split out into plugin & provider files

--- packages/vue-intl/index.ts
+++ packages/vue-intl/index.ts
@@ -1,36 +1,3 @@
-import {createIntl, IntlConfig, IntlShape} from '@formatjs/intl';
-import Vue, {inject, provide} from 'vue';
-const plugin: Vue.Plugin = {
-  install: (app, options: IntlConfig) => {
-    if (!options) {
-      throw new Error('Missing `options` for vue-intl plugin');
-    }
-    const intl = createIntl(options);
-
-    app.config.globalProperties.$intl = intl;
-    app.config.globalProperties.$formatMessage = intl.formatMessage;
-    app.config.globalProperties.$formatDate = intl.formatDate;
-    app.config.globalProperties.$formatTime = intl.formatTime;
-    app.config.globalProperties.$formatDateTimeRange = intl.formatDateTimeRange;
-    app.config.globalProperties.$formatRelativeTime = intl.formatRelativeTime;
-    app.config.globalProperties.$formatDisplayName = intl.formatDisplayName;
-    app.config.globalProperties.$formatNumber = intl.formatNumber;
-
-    app.provide('intl', intl);
-  },
-};
+import {plugin} from './plugin';
 export default plugin;
-
-const IntlProvider = Symbol();
-
-export function provideIntl(intl: IntlShape<string>) {
-  provide(IntlProvider, intl);
-}
-
-export function useIntl() {
-  const intl = inject<IntlShape<string>>(IntlProvider);
-  if (!intl) {
-    throw new Error(`provideIntl was not setup in this component's ancestor`);
-  }
-  return intl;
-}
+export * from './provider';
---
+++ packages/vue-intl/provider.ts
@@ -0,0 +1,16 @@
+import {IntlShape} from '@formatjs/intl';
+import {inject, provide} from 'vue';
+
+const IntlProvider = Symbol();
+
+export function provideIntl(intl: IntlShape<string>) {
+  provide(IntlProvider, intl);
+}
+
+export function useIntl() {
+  const intl = inject<IntlShape<string>>(IntlProvider);
+  if (!intl) {
+    throw new Error(`provideIntl was not setup in this component's ancestor`);
+  }
+  return intl;
+}
