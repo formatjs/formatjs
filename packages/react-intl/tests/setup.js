be2a3cf0128d9dece9508f8523dc9783b682624c
Long Ho <holevietlong@gmail.com>
2021-03-16 22:44:10 -0400

    fix(react-intl): trim down dependencies thanks to TS4.2

--- packages/react-intl/tests/setup.js
+++ packages/react-intl/tests/setup.js
@@ -1,11 +1,6 @@
 if (process.version.startsWith('v12')) {
-  //   delete Intl.PluralRules;
-  //   delete Intl.RelativeTimeFormat;
   delete Intl.NumberFormat
 }
-// require('@formatjs/intl-pluralrules/polyfill-locales');
-// require('@formatjs/intl-relativetimeformat/polyfill-locales');
-// require('@formatjs/intl-listformat/polyfill-locales');
 require('@formatjs/intl-displaynames/polyfill-locales')
 require('@formatjs/intl-numberformat/polyfill')
 require('@formatjs/intl-numberformat/locale-data/en')
