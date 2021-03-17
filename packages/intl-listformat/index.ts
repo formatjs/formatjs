888067ae6fef5ae9d750ab9ef60a44600841f2ab
Long Ho <holevietlong@gmail.com>
2021-03-16 22:33:51 -0400

    fix(@formatjs/intl-listformat): fix type doc, fix #2716

--- packages/intl-listformat/index.ts
+++ packages/intl-listformat/index.ts
@@ -24,19 +24,20 @@ export interface IntlListFormatOptions {
    */
   localeMatcher?: 'best fit' | 'lookup'
   /**
-   * The format of output message. Possible values are:
-   * - "always" (default, e.g., 1 day ago),
-   * - or "auto" (e.g., yesterday).
-   * The "auto" value allows to not always have to
-   * use numeric values in the output.
+   * The format of output message.
+   * Possible values are :
+   * - "conjunction" that stands for "and"-based lists (default, e.g., "A, B, and C")
+   * - "disjunction" that stands for "or"-based lists (e.g., "A, B, or C").
+   * - "unit" stands for lists of values with units (e.g., "5 pounds, 12 ounces").
    */
   type?: 'conjunction' | 'disjunction' | 'unit'
   /**
-   * The length of the internationalized message. Possible values are:
-   * - "long" (default, e.g., in 1 month)
-   * - "short" (e.g., in 1 mo.),
-   * - or "narrow" (e.g., in 1 mo.).
-   * The narrow style could be similar to the short style for some locales.
+   * The length of the formatted message.
+   * Possible values are:
+   * - "long" (default, e.g., "A, B, and C");
+   * - "short" (e.g., "A, B, C"), or
+   * - "narrow" (e.g., "A B C").
+   * When style is "short" or "narrow", "unit" is the only allowed value for the type option.
    */
   style?: 'long' | 'short' | 'narrow'
 }
