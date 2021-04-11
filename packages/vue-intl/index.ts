9123574129641eef69795147a265c9c291d87da5
Long Ho <holevietlong@gmail.com>
2021-04-10 22:34:24 -0400

    fix(vue-intl): export the same stuff as react-intl

--- packages/vue-intl/index.ts
+++ packages/vue-intl/index.ts
@@ -1,3 +1,40 @@
+import {MessageDescriptor} from '@formatjs/intl'
 import {plugin} from './plugin'
 export default plugin
 export * from './provider'
+export {
+  IntlShape,
+  IntlConfig,
+  createIntlCache,
+  MessageDescriptor,
+  IntlCache,
+  Formatters,
+  IntlFormatters,
+  FormatDisplayNameOptions,
+  FormatListOptions,
+  FormatPluralOptions,
+  FormatRelativeTimeOptions,
+  FormatNumberOptions,
+  FormatDateOptions,
+  CustomFormatConfig,
+  CustomFormats,
+  UnsupportedFormatterError,
+  InvalidConfigError,
+  MissingDataError,
+  MessageFormatError,
+  MissingTranslationError,
+  IntlErrorCode,
+  IntlError,
+} from '@formatjs/intl'
+export function defineMessages<
+  K extends keyof any,
+  T = MessageDescriptor,
+  U extends Record<K, T> = Record<K, T>
+>(msgs: U): U {
+  return msgs
+}
+
+export function defineMessage<T>(msg: T): T {
+  return msg
+}
+export type {MessageFormatElement} from '@formatjs/icu-messageformat-parser'
