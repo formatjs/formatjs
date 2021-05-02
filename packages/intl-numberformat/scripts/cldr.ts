121e4c676bf67be2540c41971c732bba86fd806b
Long Ho <holevietlong@gmail.com>
2021-05-01 21:35:00 -0400

    fix(@formatjs/intl-numberformat): add d.ts for locale files, fix #2850

--- packages/intl-numberformat/scripts/cldr.ts
+++ packages/intl-numberformat/scripts/cldr.ts
@@ -14,15 +14,15 @@ function main(args: minimist.ParsedArgs) {
   // Dist all locale files to locale-data (JS)
   for (const locale of locales) {
     const data = readFileSync(join(cldrFolder, `${locale}.json`))
-    const destFile = join(outDir, locale + '.js')
     outputFileSync(
-      destFile,
+      join(outDir, locale + '.js'),
       `/* @generated */
 // prettier-ignore
 if (Intl.NumberFormat && typeof Intl.NumberFormat.__addLocaleData === 'function') {
   Intl.NumberFormat.__addLocaleData(${data})
 }`
     )
+    outputFileSync(join(outDir, locale + '.d.ts'), 'export {}')
   }
 }
 
