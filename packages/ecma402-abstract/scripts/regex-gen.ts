c991b8c4bb1b8244eb7fd5490b75727576e1bbd9
Long Ho <holevietlong@gmail.com>
2021-05-16 23:26:09 -0400

    feat(@formatjs/ecma402-abstract): upgrade symbol regex to unicode 13

---
+++ packages/ecma402-abstract/scripts/regex-gen.ts
@@ -0,0 +1,20 @@
+import './global'
+import regenerate from 'regenerate'
+import {outputFileSync} from 'fs-extra'
+import minimist from 'minimist'
+
+function main(args: minimist.ParsedArgs) {
+  const symbolSeparator = regenerate().add(
+    require('@unicode/unicode-13.0.0/General_Category/Symbol/code-points.js')
+  )
+  outputFileSync(
+    args.out,
+    `// @generated from regex-gen.ts
+export const S_UNICODE_REGEX = /${symbolSeparator.toString()}/
+`
+  )
+}
+
+if (require.main === module) {
+  main(minimist(process.argv))
+}
