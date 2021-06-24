d6f29ab77c8196ff01d98bfd9fc6f36dd5c27b5e
GSingh <71792252+Guriqbal-Singh-Alida@users.noreply.github.com>
2021-06-24 13:51:17 -0700

    #3000 : Fixes issue

--- packages/ts-transformer/ts-jest-integration.ts
+++ packages/ts-transformer/ts-jest-integration.ts
@@ -5,5 +5,5 @@ export const name = '@formatjs/ts-transformer'
 export const version = '2.10.1'
 
 export function factory(compilerInstance: TsCompilerInstance, opts: Opts) {
-  return transformWithTs(compilerInstance.configSet.compilerModule, opts)
+  return transformWithTs(compilerInstance.configSet ? compilerInstance.configSet.compilerModule : compilerInstance.compilerModule, opts)
 }
