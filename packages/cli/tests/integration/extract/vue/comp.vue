085ac9d6a5befe06da892ea2e8e52f3a5d3c4a65
Long Ho <holevietlong@gmail.com>
2021-06-26 10:26:10 -0400

    refactor(@formatjs/cli): split out integration test

---
+++ packages/cli/tests/integration/extract/vue/comp.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>
+    {{
+      $formatMessage({
+        defaultMessage: 'in template',
+        description: 'in template desc',
+      })
+    }}
+  </p>
+</template>
+
+<script>
+intl.formatMessage({
+  defaultMessage: 'in script',
+  description: 'in script desc',
+})
+</script>
