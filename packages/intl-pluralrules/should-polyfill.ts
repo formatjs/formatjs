395efcb843c1d20ca11b55375d4b2536a415a750
Long Ho <holevietlong@gmail.com>
2021-07-23 23:12:55 -0400

    feat(@formatjs/intl-pluralrules): add supported locales check to should-polyfill, fix #3066

--- packages/intl-pluralrules/should-polyfill.ts
+++ packages/intl-pluralrules/should-polyfill.ts
@@ -1,7 +1,16 @@
-export function shouldPolyfill() {
+function supportedLocalesOf(locale?: string | string[]) {
+  if (!locale) {
+    return true
+  }
+  const locales = Array.isArray(locale) ? locale : [locale]
+  return Intl.PluralRules.supportedLocalesOf(locales).length === locales.length
+}
+
+export function shouldPolyfill(locale?: string | string[]) {
   return (
     !('PluralRules' in Intl) ||
     new Intl.PluralRules('en', {minimumFractionDigits: 2} as any).select(1) ===
-      'one'
+      'one' ||
+    !supportedLocalesOf(locale)
   )
 }
