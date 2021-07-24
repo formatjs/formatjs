ab90bf53b9fee8645e93656517876d93d93b12c3
Long Ho <holevietlong@gmail.com>
2021-07-23 23:13:10 -0400

    feat(@formatjs/intl-relativetimeformat): add supported locales check to should-polyfill, fix #3066

--- packages/intl-relativetimeformat/should-polyfill.ts
+++ packages/intl-relativetimeformat/should-polyfill.ts
@@ -1,3 +1,14 @@
-export function shouldPolyfill() {
-  return !('RelativeTimeFormat' in Intl)
+function supportedLocalesOf(locale?: string | string[]) {
+  if (!locale) {
+    return true
+  }
+  const locales = Array.isArray(locale) ? locale : [locale]
+  return (
+    Intl.RelativeTimeFormat.supportedLocalesOf(locales).length ===
+    locales.length
+  )
+}
+
+export function shouldPolyfill(locale?: string | string[]) {
+  return !('RelativeTimeFormat' in Intl) || !supportedLocalesOf(locale)
 }
