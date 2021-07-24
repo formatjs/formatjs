4640c81df591a126ecc33d919884c219e0c3f58d
Long Ho <holevietlong@gmail.com>
2021-07-23 23:12:23 -0400

    feat(@formatjs/intl-listformat): add supported locales check to should-polyfill, fix #3066

--- packages/intl-listformat/should-polyfill.ts
+++ packages/intl-listformat/should-polyfill.ts
@@ -1,3 +1,14 @@
-export function shouldPolyfill() {
-  return !('ListFormat' in Intl)
+function supportedLocalesOf(locale?: string | string[]) {
+  if (!locale) {
+    return true
+  }
+  const locales = Array.isArray(locale) ? locale : [locale]
+  return (
+    (Intl as any).ListFormat.supportedLocalesOf(locales).length ===
+    locales.length
+  )
+}
+
+export function shouldPolyfill(locale?: string | string[]) {
+  return !('ListFormat' in Intl) || !supportedLocalesOf(locale)
 }
