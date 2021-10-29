4bbbc0bb74775addecf74b60e6995f2bfcedde12
Guille De Fermin <guillermo.defermin@taxfix.de>
2021-10-29 10:50:24 +0200

    docs(react-intl): add example usage in react-intl

---
+++ packages/react-intl/example-sandboxes/strict-message-types/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './styles.css'
+import {IntlProvider, FormattedMessage} from 'react-intl'
+
+const messages = {
+  title: 'Hello CodeSandbox',
+  content: 'Start editing to see some magic happen!',
+}
+
+declare global {
+  namespace FormatjsIntl {
+    interface Message {
+      ids: keyof typeof messages
+    }
+  }
+}
+
+export default function App() {
+  return (
+    <IntlProvider locale="en" messages={messages}>
+      <div className="App">
+        <h1>
+          <FormattedMessage id="title" />
+        </h1>
+        <h2>
+          <FormattedMessage id="content" />
+        </h2>
+      </div>
+    </IntlProvider>
+  )
+}
