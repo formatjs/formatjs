587e0f1a5563a9ba0effceb2801fb11e0061be11
Long Ho <holevietlong@gmail.com>
2022-03-25 21:11:23 -0400

    fix(@formatjs/intl-getcanonicallocales): rm @types/node dep, fix #3474

--- packages/intl-getcanonicallocales/polyfill.ts
+++ packages/intl-getcanonicallocales/polyfill.ts
@@ -5,7 +5,9 @@ if (typeof Intl === 'undefined') {
     Object.defineProperty(window, 'Intl', {
       value: {},
     })
+    // @ts-ignore we don't include @types/node so global isn't a thing
   } else if (typeof global !== 'undefined') {
+    // @ts-ignore we don't include @types/node so global isn't a thing
     Object.defineProperty(global, 'Intl', {
       value: {},
     })
