a5da0375a1703f60be1da06903e0982ddfc3b06e
Linjie Ding <pyrocat101@users.noreply.github.com>
2022-06-20 20:47:21 -0800

    feat(eslint-plugin-formatjs): introduce no-literal-string-in-jsx (#3674)
    This prevents untranslated strings in JSX.
    
    # Why
    
    - It is easy to forget wrapping JSX text in translation functions or components.
    - It is easy to forget wrapping certain accessibility attributes (e.g. `aria-label`) in translation functions.
    
    ```tsx
    // WORKS
    <Button>
      <FormattedMessage defaultMessage="Submit" />
    </Button>
    // WORKS
    <Button>
      {customTranslateFn("Submit")}
    </Button>
    // WORKS
    <input aria-label={intl.formatMessage({defaultMessage: "Label"})} />
    // WORKS
    <img
      src="/example.png"
      alt={intl.formatMessage({defaultMessage: "Image description"})}
    />
    // FAILS
    <Button>Submit</Button>
    // FAILS
    <Button>{'Submit'}</Button>
    // FAILS
    <Button>{`Te` + 's' + t}</Button>
    // FAILS
    <input aria-label="Untranslated label" />
    // FAILS
    <img src="/example.png" alt="Image description" />
    // FAILS
    <input aria-label={`Untranslated label`} />
    ```
    
    This linter reporter text literals or string expressions, including string
    concatenation expressions in the JSX children. It also checks certain JSX
    attributes that you can customize. Example:
    
    ```javascript
    // In .eslintrc.js
    module.exports = {
      // other rules...
      'formatjs/no-literal-string-in-jsx': [
        2,
        {
          // Include or exclude additional prop checks (merged with the default checks)
          props: {
            include: [
              // picomatch style glob pattern for tag name and prop name.
              // check `name` prop of `UI.Button` tag.
              ['UI.Button', 'name'],
              // check `message` of any component.
              ['*', 'message'],
            ],
            // Exclude will always override include.
            exclude: [
              // do not check `message` of the `Foo` tag.
              ['Foo', 'message'],
              // do not check aria-label and aria-description of `Bar` tag.
              ['Bar', 'aria-{label,description}'],
            ],
          },
        },
      ],
    }
    ```
    
    The default prop checks are:
    
    ```jsx
    {
      include: [
        // check aria attributes that the screen reader announces.
        ['*', 'aria-{label,description,details,errormessage}'],
        // check placeholder and title attribute of all native DOM elements.
        ['[a-z]*([a-z0-9])', '(placeholder|title)'],
        // check alt attribute of the img tag.
        ['img', 'alt'],
      ],
      exclude: []
    }
    ```

---
+++ packages/eslint-plugin-formatjs/tests/no-literal-string-in-jsx.test.ts
@@ -0,0 +1,213 @@
+import noLiteralStringInJsx from '../rules/no-literal-string-in-jsx'
+import {ruleTester} from './util'
+
+ruleTester.run('no-literal-string-in-jsx', noLiteralStringInJsx, {
+  valid: [
+    {
+      code: '<FormattedMessage defaultMessage="Test" />',
+    },
+    {
+      code: '<div aria-label={f("label")} />',
+    },
+    {
+      code: '<img alt={f("alt")} />',
+    },
+    {
+      code: '<div>{f("message")}</div>',
+    },
+    // Excluded built-in img alt attribute check
+    {
+      code: '<img alt="alt" />',
+      options: [
+        {
+          props: {
+            exclude: [['img', 'alt']],
+          },
+        },
+      ],
+    },
+    // Excluded built-in aria-label check.
+    {
+      code: `
+        <>
+          <Foo aria-label="test" />
+          <Bar aria-label="test" />
+        </>
+      `,
+      options: [
+        {
+          props: {
+            exclude: [['*', 'aria-label']],
+          },
+        },
+      ],
+    },
+    // Exclusion works on member expression and type parameters
+    {
+      code: '<UI.Button<T> aria-label="test" />',
+      options: [
+        {
+          props: {
+            exclude: [['*', 'aria-label']],
+          },
+        },
+      ],
+    },
+    // Exclusion overrides inclusion
+    {
+      code: '<UI.Button<T> name="test" />',
+      options: [
+        {
+          props: {
+            include: [['*', 'name']],
+            exclude: [['UI.Button', 'name']],
+          },
+        },
+      ],
+    },
+    {
+      code: '<input type="text" placeholder={f("foo")} aria-label={f("bar")} />',
+    },
+    {
+      code: `
+        <Component
+          aria-label={intl.formatMessage({defaultMessage: "test"})}
+          name={intl.formatMessage({defaultMessage: "test"})}
+        >
+          <FormattedMessage defaultMessage="test" />
+        </Component>
+      `,
+      options: [
+        {
+          props: {
+            include: [['Component', 'name']],
+          },
+        },
+      ],
+    },
+  ],
+  invalid: [
+    {
+      code: '<h1>Test</h1>',
+      errors: [{message: 'Cannot have untranslated text in JSX'}],
+    },
+    {
+      code: '<img src="/example.png" alt="Example" />',
+      errors: [{message: 'Cannot have untranslated text in JSX'}],
+    },
+    {
+      code: '<img src="/example.png" alt={"Example"} />',
+      errors: [{message: 'Cannot have untranslated text in JSX'}],
+    },
+    {
+      code: '<img src="/example.png" alt={"Exa" + `mple`} />',
+      errors: [
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+      ],
+    },
+    {
+      code: '<img src="/example.png" alt={`Example`} />',
+      errors: [{message: 'Cannot have untranslated text in JSX'}],
+    },
+    {
+      code: '<div title="title" />',
+      errors: [{message: 'Cannot have untranslated text in JSX'}],
+    },
+    {
+      code: '<div title="title">text</div>',
+      errors: [
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+      ],
+    },
+    {
+      code: '<div aria-label="test">test</div>',
+      errors: [
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+      ],
+    },
+    {
+      code: '<div aria-description="test">test</div>',
+      errors: [
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+      ],
+    },
+    {
+      code: '<div>{"foo"}</div>',
+      errors: [{message: 'Cannot have untranslated text in JSX'}],
+    },
+    {
+      code: `
+        <div>
+          {\`foo\` + x}
+          bar
+          {'baz' + 'qux'}
+        </div>
+      `,
+      errors: [
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+      ],
+    },
+    {
+      code: `
+        <div>
+          foo
+          <div>
+            bar
+          </div>
+        </div>
+      `,
+      errors: [
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+      ],
+    },
+    // Inclusion works
+    {
+      code: '<UI.Button<T> name="test" />',
+      options: [
+        {
+          props: {
+            include: [['UI.Button', 'name']],
+          },
+        },
+      ],
+      errors: [{message: 'Cannot have untranslated text in JSX'}],
+    },
+    {
+      code: '<UI.Button<T> name="test" />',
+      options: [
+        {
+          props: {
+            include: [['*', 'name']],
+            // Exclude should match the entire member expression. So this one is a miss.
+            exclude: [['Button', 'name']],
+          },
+        },
+      ],
+      errors: [{message: 'Cannot have untranslated text in JSX'}],
+    },
+    // Multiple
+    {
+      code: '<UI.Button<T> aria-label={`label`} aria-description="description">Child</UI.Button>',
+      errors: [
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+      ],
+    },
+    {
+      code: '<input type="text" placeholder="foo" aria-label="bar" />',
+      errors: [
+        {message: 'Cannot have untranslated text in JSX'},
+        {message: 'Cannot have untranslated text in JSX'},
+      ],
+    },
+  ],
+})
