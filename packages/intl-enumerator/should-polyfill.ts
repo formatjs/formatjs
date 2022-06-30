8d6f80a0793f0c6ad6b82e7cf0a21d9735ee3e7a
Michele Riva <ciao@micheleriva.it>
2022-06-30 16:41:08 +0200

    feat: porting of proposal-intl-enumeration to typescript

---
+++ packages/intl-enumerator/should-polyfill.ts
@@ -0,0 +1,12 @@
+type AvailablePolyfills =
+  | 'getSupportedCalendars'
+  | 'getSupportedCurrencies'
+  | 'getSupportedNumberingSystems'
+  | 'getSupportedRegions'
+  | 'getSupportedScripts'
+  | 'getSupportedTimeZones'
+  | 'getSupportedUnits';
+
+export function shouldPolyfill(polyfill: AvailablePolyfills): boolean {
+  return !(polyfill in Intl);
+}
---
+++ packages/intl-enumerator/tests/getSupportedCalendars.test.ts
@@ -0,0 +1,4 @@
+describe('getSupportedCalendars', () => {
+  // @todo implement tests, this is a placeholder for Bazel
+  expect('getSupportedCalendars').toBe('getSupportedCalendars');
+})
