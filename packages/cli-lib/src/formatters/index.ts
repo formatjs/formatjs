2ce952c29cdc2ab5a913243ef89af7885463d8ad
Martin Chrástek <chrastek12@gmail.com>
2022-08-05 04:18:06 +0200

    fix(@formatjs/cli-lib): formatter path on Windows (#3741)

--- packages/cli-lib/src/formatters/index.ts
+++ packages/cli-lib/src/formatters/index.ts
@@ -7,6 +7,7 @@ import * as lokalise from './lokalise'
 import * as crowdin from './crowdin'
 import {Comparator} from 'json-stable-stringify'
 import {resolve} from 'path'
+import {pathToFileURL} from 'url'
 
 export interface Formatter {
   format: FormatFn
@@ -37,7 +38,7 @@ export async function resolveBuiltinFormatter(
   }
   try {
     // eslint-disable-next-line import/dynamic-import-chunkname
-    return import(resolve(process.cwd(), format))
+    return import(pathToFileURL(resolve(process.cwd(), format)).href)
   } catch (e) {
     console.error(`Cannot resolve formatter ${format}`)
     throw e
