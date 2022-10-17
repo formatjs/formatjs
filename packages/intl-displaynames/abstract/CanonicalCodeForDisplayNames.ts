fd214478c5d3311480992735ffa1ef8af1329639
Andrea Forni <andrea.forni@nearform.com>
2022-10-17 13:11:15 +0200

    feat(@formatjs/intl-displaynames): add to CanonicalCodeForDisplayNames the new type "calendar" and "dateTimeField". Implement the new IsValidDateTimeFieldCode abstract operation. Add tests

--- packages/intl-displaynames/abstract/CanonicalCodeForDisplayNames.ts
+++ packages/intl-displaynames/abstract/CanonicalCodeForDisplayNames.ts
@@ -4,8 +4,12 @@ import {
   IsWellFormedCurrencyCode,
 } from '@formatjs/ecma402-abstract'
 
+import {IsValidDateTimeFieldCode} from './IsValidDateTimeFieldCode'
+
 const UNICODE_REGION_SUBTAG_REGEX = /^([a-z]{2}|[0-9]{3})$/i
 const ALPHA_4 = /^[a-z]{4}$/i
+const UNICODE_TYPE_REGEX = /^[a-z0-9]{3,8}([-_][a-z0-9]{3,8})*$/i
+
 function isUnicodeRegionSubtag(region: string): boolean {
   return UNICODE_REGION_SUBTAG_REGEX.test(region)
 }
@@ -14,8 +18,18 @@ function isUnicodeScriptSubtag(script: string): boolean {
   return ALPHA_4.test(script)
 }
 
+function isUnicodeLocaleIdentifierType(code: string): boolean {
+  return UNICODE_TYPE_REGEX.test(code)
+}
+
 export function CanonicalCodeForDisplayNames(
-  type: 'language' | 'region' | 'script' | 'currency',
+  type:
+    | 'language'
+    | 'region'
+    | 'script'
+    | 'calendar'
+    | 'dateTimeField'
+    | 'currency',
   code: string
 ): string {
   if (type === 'language') {
@@ -32,6 +46,18 @@ export function CanonicalCodeForDisplayNames(
       throw RangeError('invalid script')
     }
     return `${code[0].toUpperCase()}${code.slice(1).toLowerCase()}`
+  }
+  if (type === 'calendar') {
+    if (!isUnicodeLocaleIdentifierType(code)) {
+      throw RangeError('invalid calendar')
+    }
+    return code.toLowerCase()
+  }
+  if (type === 'dateTimeField') {
+    if (!IsValidDateTimeFieldCode(code)) {
+      throw RangeError('invalid dateTimeField')
+    }
+    return code
   }
   invariant(type === 'currency', 'invalid type')
   if (!IsWellFormedCurrencyCode(code)) {
