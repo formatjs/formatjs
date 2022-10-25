688cdec375e9c82e049804f65a001525afcdb627
Andreas Loew <andreas@codeandweb.com>
2022-10-25 14:32:35 +0200

    Allow format name `default` when setting the format for extraction using `--format`.

--- packages/cli-lib/src/formatters/index.ts
+++ packages/cli-lib/src/formatters/index.ts
@@ -35,6 +35,8 @@ export async function resolveBuiltinFormatter(
       return lokalise
     case 'crowdin':
       return crowdin
+    case 'default':
+      return defaultFormatter
   }
   try {
     // eslint-disable-next-line import/dynamic-import-chunkname
