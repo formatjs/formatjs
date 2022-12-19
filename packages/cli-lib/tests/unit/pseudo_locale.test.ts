7447f8abd931de9bd8baf7850ff09e57f38c40fd
Linjie Ding <pyrocat101@users.noreply.github.com>
2022-12-19 17:57:20 -0500

    fix(@formatjs/cli-lib): fix incorrect xx-HA generation

--- packages/cli-lib/tests/unit/pseudo_locale.test.ts
+++ packages/cli-lib/tests/unit/pseudo_locale.test.ts
@@ -1,7 +1,17 @@
 import {generateXXLS} from '../../src/pseudo_locale'
 
-describe.only('pseudo-locale: xx-LS', () => {
+describe('pseudo-locale: xx-LS', () => {
   it('works with messages that ends with a tag', () => {
     expect(generateXXLS('Foo <a>bar</a>')).toMatchSnapshot()
+  })
+
+  it('works with message that does not contain literal element', () => {
+    expect(generateXXLS('{foo}')).toMatchSnapshot()
+  })
+})
+
+describe('pseudo-locale: en-HA', () => {
+  it('works with message that does not contain literal element', () => {
+    expect(generateXXLS('{foo}')).toMatchSnapshot()
   })
 })
