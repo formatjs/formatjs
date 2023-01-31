32628df8bdadc65c0417880fa923c2c0e9efe39e
Linjie Ding <pyrocat101@users.noreply.github.com>
2023-01-31 17:39:23 -0500

    feat!(@formatjs/cli-lib): repurpose en-XB for bidi pseudo locale (#3978)
    BREAKING CHANGE: `en-XA` is a pseudo locale for accented and lengthned English with markers, and `en-XB` is now a bidi pseudo locale.

--- packages/cli-lib/tests/unit/pseudo_locale.test.ts
+++ packages/cli-lib/tests/unit/pseudo_locale.test.ts
@@ -1,21 +1,45 @@
-import {generateXXHA, generateXXLS} from '../../src/pseudo_locale'
+import {
+  generateXXAC,
+  generateXXHA,
+  generateXXLS,
+  generateENXA,
+  generateENXB,
+} from '../../src/pseudo_locale'
 
-describe('pseudo-locale: xx-LS', () => {
-  it('works with messages that ends with a tag', () => {
-    expect(generateXXLS('Foo <a>bar</a>')).toMatchSnapshot()
-  })
+const pseudoLocales = [
+  {name: 'en-LS', generate: generateXXLS},
+  {name: 'en-AC', generate: generateXXAC},
+  {name: 'en-HA', generate: generateXXHA},
+  {name: 'en-XA', generate: generateENXA},
+  {name: 'en-XB', generate: generateENXB},
+]
 
-  it('works with message that does not contain literal element', () => {
-    expect(generateXXLS('{foo} bar')).toMatchSnapshot()
-  })
-})
+for (const {name, generate} of pseudoLocales) {
+  describe(`pseudo-locale ${name}`, () => {
+    it('works with a string literal', () => {
+      expect(generate('Find Help Online')).toMatchSnapshot()
+    })
 
-describe('pseudo-locale: en-HA', () => {
-  it('works with message that does not start with a literal element', () => {
-    expect(generateXXHA('{foo} bar')).toMatchSnapshot()
-  })
+    it('works with message that does not start with a literal element', () => {
+      expect(generate('{foo} bar')).toMatchSnapshot()
+    })
 
-  it('works with a message that starts with a literal element', () => {
-    expect(generateXXHA('foo {bar}')).toMatchSnapshot()
+    it('works with a message that starts with a literal element', () => {
+      expect(generate('foo {bar}')).toMatchSnapshot()
+    })
+
+    it('works with a message that has a plural element', () => {
+      expect(
+        generate('foo {bar, plural, =1 {1 bar} other {# bars}}')
+      ).toMatchSnapshot()
+    })
+
+    it('works with messages that ends with a tag', () => {
+      expect(generate('Foo <a>bar</a>')).toMatchSnapshot()
+    })
+
+    it('works with message that does not contain a literal element', () => {
+      expect(generate('{foo}')).toMatchSnapshot()
+    })
   })
-})
+}
