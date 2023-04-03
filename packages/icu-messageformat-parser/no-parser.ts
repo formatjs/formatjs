ecf70f347241159aa785635e02d33d0f645d617f
Linjie Ding <pyrocat101@users.noreply.github.com>
2023-04-03 18:56:22 -0400

    fix(react-intl): fix a typo in react-intl/no-parser

--- packages/icu-messageformat-parser/no-parser.ts
+++ packages/icu-messageformat-parser/no-parser.ts
@@ -1,6 +1,6 @@
 export function parse() {
   throw new Error(
-    "You're trying to format an uncompiled message with react-intl without parser, please import from 'react-int' instead"
+    "You're trying to format an uncompiled message with react-intl without parser, please import from 'react-intl' instead"
   )
 }
 export * from './types'
