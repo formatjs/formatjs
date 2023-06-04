1ebf88f3fd4482e2a4a4c8e27d606e0f22065d48
Linjie Ding <pyrocat101@users.noreply.github.com>
2023-06-04 16:19:33 -0400

    chore: use bazel-super-formatter to format all source files

--- rust/swc-formatjs-custom-transform/index.d.ts
+++ rust/swc-formatjs-custom-transform/index.d.ts
@@ -7,7 +7,12 @@ export interface TransformOutput {
   code: string
   map?: string
 }
-export function transformSync(s: string, isModule: boolean, opts: Buffer, instrumentOpts: Buffer): TransformOutput
+export function transformSync(
+  s: string,
+  isModule: boolean,
+  opts: Buffer,
+  instrumentOpts: Buffer
+): TransformOutput
 export type JsCompiler = Compiler
 export class Compiler {
   constructor()
