44a096af7532811b723298260cd33e1169baf731
Long Ho <long.ho@cloudkitchens.com>
2023-11-12 23:03:53 -0500

    docs: fix live editor, fix #4259

--- website/src/theme/Playground/index.js
+++ website/src/theme/Playground/index.js
@@ -1,14 +1,7 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-import * as React from 'react'
-import {LiveProvider, LiveEditor, LiveError, LivePreview} from 'react-live'
 import clsx from 'clsx'
+import * as React from 'react'
 import {IntlProvider} from 'react-intl'
+import {LiveEditor, LiveError, LivePreview, LiveProvider} from 'react-live'
 
 import styles from './styles.module.css'
 
@@ -16,7 +9,7 @@ function Playground({children, theme, transformCode, ...props}) {
   return (
     <LiveProvider
       code={children.replace(/\n$/, '')}
-      transformCode={transformCode || (code => `${code};`)}
+      transformCode={transformCode || (code => `<>{${code}}</>`)}
       theme={theme}
       {...props}
     >
--- website/src/theme/ReactLiveScope/index.js
+++ website/src/theme/ReactLiveScope/index.js
@@ -1,14 +1,7 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
+import * as IcuMessageFormatParser from '@formatjs/icu-messageformat-parser'
+import IntlMessageFormat from 'intl-messageformat'
 import React from 'react'
 import * as ReactIntl from 'react-intl'
-import IntlMessageFormat from 'intl-messageformat'
-import * as IcuMessageFormatParser from '@formatjs/icu-messageformat-parser'
 
 // Add react-live imports you need here
 const ReactLiveScope = {
