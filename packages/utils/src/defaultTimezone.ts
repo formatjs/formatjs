a53d0a2e4112816d453bb65eb2500b7a72143011
longlho <holevietlong@gmail.com>
2024-11-04 11:22:08 -0500

    feat(@formatjs/utils): add ability to return localized default timezone

--- packages/utils/src/defaultTimezone.ts
+++ packages/utils/src/defaultTimezone.ts
@@ -1,31 +1,58 @@
-let DEFAULT_TIMEZONE: string | undefined
+import {Cache, memoize, strategies} from '@formatjs/fast-memoize'
 
-const DEFAULT_DATETIMEFORMAT_CREATOR = () => new Intl.DateTimeFormat()
+const cache: Record<string, Intl.DateTimeFormat> = {}
+
+function createFastMemoizeCache<V>(
+  store: Record<string, V | undefined>
+): Cache<string, V> {
+  return {
+    create() {
+      return {
+        get(key) {
+          return store[key]
+        },
+        set(key, value) {
+          store[key] = value
+        },
+      }
+    },
+  }
+}
+
+type DTFParameters = ConstructorParameters<typeof Intl.DateTimeFormat>
+
+const getDateTimeFormat: (...args: DTFParameters) => Intl.DateTimeFormat =
+  memoize((...args: DTFParameters) => new Intl.DateTimeFormat(...args), {
+    cache: createFastMemoizeCache(cache),
+    strategy: strategies.variadic,
+  })
+
+const now = Date.now()
 
 /**
- * Return the default timezone for the system.
+ * Return the localized default timezone for the system. If `timeZoneName` is not supported, return the IANA timezone name.
  * @param dateDateTimeFormatCreator creator fn for Intl.DateTimeFormat
  * @returns default timezone for the system
  */
 export function defaultTimezone(
-  dateDateTimeFormatCreator = DEFAULT_DATETIMEFORMAT_CREATOR
+  locales?: DTFParameters[0],
+  options?: Pick<Intl.DateTimeFormatOptions, 'timeZoneName'>
 ): string {
-  if (DEFAULT_TIMEZONE) {
-    return DEFAULT_TIMEZONE
-  }
+  const timeZoneName = options?.timeZoneName
   try {
-    return (DEFAULT_TIMEZONE =
-      dateDateTimeFormatCreator().resolvedOptions().timeZone)
+    const dtf = getDateTimeFormat(locales, {
+      timeZoneName: options?.timeZoneName,
+    })
+    // If there's no `timeZoneName` specified, return the IANA timezone name
+    if (!timeZoneName) {
+      return dtf.resolvedOptions().timeZone
+    }
+    // If `timeZoneName` is specified, return the localized timezone name
+    return (
+      dtf.formatToParts(now).find(p => p.type === 'timeZoneName')?.value ||
+      'UTC'
+    )
   } catch (e) {
     return 'UTC'
   }
-}
-
-/**
- * Reset the default timezone.
- * Only for testing purposes.
- * @private
- */
-export function _resetDefaultTimezone() {
-  DEFAULT_TIMEZONE = undefined
 }
--- packages/utils/tests/defaultTimezone.test.ts
+++ packages/utils/tests/defaultTimezone.test.ts
@@ -1,29 +1,13 @@
-import {_resetDefaultTimezone, defaultTimezone} from '../src/defaultTimezone'
+import {defaultTimezone} from '../src/defaultTimezone'
 
 describe('defaultTimezone', () => {
-  beforeEach(() => {
-    _resetDefaultTimezone()
-  })
   it('returns the default timezone', () => {
     expect(defaultTimezone().length).toBeGreaterThan(0)
   })
-  it('should not throw', () => {
-    expect(
-      defaultTimezone(() => {
-        throw new Error()
-      })
-    ).toBe('UTC')
+  it('should return localized timezone name', () => {
+    expect(defaultTimezone('it', {timeZoneName: 'longGeneric'})).toBe('GMT')
   })
-  it('should prioritize default platform timezone', () => {
-    const spyFn = jest.fn(() => {
-      return {
-        resolvedOptions() {
-          return {
-            timeZone: 'blah',
-          }
-        },
-      }
-    })
-    expect(defaultTimezone(spyFn as any)).toBe('blah')
+  it('should return IANA timezone name if timeZoneName is not specified', () => {
+    expect(defaultTimezone('it')).toBe('UTC')
   })
 })
