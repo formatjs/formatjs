bda0d1fdbadfb54abf3c1be2396e4c5120f0b781
longlho <holevietlong@gmail.com>
2024-12-07 11:25:28 -0500

    feat(@formatjs/cli-lib): change main entry point to esm
    BREAKING CHANGE: Due to downstream deps changes in Vue we have to change main
    entry point to esm esnext.

--- packages/cli-lib/src/gts_extractor.ts
+++ packages/cli-lib/src/gts_extractor.ts
@@ -7,7 +7,7 @@ export function parseFile(source: string, fileName: string, options: any) {
   const scriptParseFn = parseScript(options, fileName)
   const transformedSource = p.process(source, {filename: fileName})
 
-  scriptParseFn(transformedSource)
+  scriptParseFn(transformedSource.code)
 
   // extract template from transformed source to then run through hbs processor
   const parseResult = p.parse(source, {filename: fileName})
