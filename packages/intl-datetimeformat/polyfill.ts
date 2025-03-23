22a4be46b8c9ca144fcc01c96d6bcb6b89462aeb
Long Ho <holevietlong@gmail.com>
2025-03-22 22:05:24 -0400

    fix(@formatjs/intl-datetimeformat): consistent polyfill with polyfill-force

--- packages/intl-datetimeformat/polyfill.ts
+++ packages/intl-datetimeformat/polyfill.ts
@@ -12,7 +12,10 @@ if (shouldPolyfill()) {
   defineProperty(Date.prototype, 'toLocaleString', {
     value: function toLocaleString(
       locales?: string | string[],
-      options?: Intl.DateTimeFormatOptions
+      options: Intl.DateTimeFormatOptions = {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      }
     ) {
       try {
         return _toLocaleString(this, locales, options)
@@ -24,7 +27,9 @@ if (shouldPolyfill()) {
   defineProperty(Date.prototype, 'toLocaleDateString', {
     value: function toLocaleDateString(
       locales?: string | string[],
-      options?: Intl.DateTimeFormatOptions
+      options: Intl.DateTimeFormatOptions = {
+        dateStyle: 'short',
+      }
     ) {
       try {
         return _toLocaleDateString(this, locales, options)
@@ -36,7 +41,9 @@ if (shouldPolyfill()) {
   defineProperty(Date.prototype, 'toLocaleTimeString', {
     value: function toLocaleTimeString(
       locales?: string | string[],
-      options?: Intl.DateTimeFormatOptions
+      options: Intl.DateTimeFormatOptions = {
+        timeStyle: 'medium',
+      }
     ) {
       try {
         return _toLocaleTimeString(this, locales, options)
