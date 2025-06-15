be979961e54ea8c5369e8631a293909f5a5dcd61
Long Ho <longlho@users.noreply.github.com>
2025-06-15 10:58:00 -0400

    feat(eslint-plugin-formatjs): support tagged template expression like dedent (#5016)
    # feat(@formatjs/ts-transformer): support tagged template expression like dedent
    
    This PR adds support for tagged template expressions (like `dedent`) in message descriptors for both the ESLint plugin and TS transformer. Now you can use tagged template literals like `dedent` to format your messages while maintaining proper extraction and validation.
    
    The implementation handles tagged templates in:
    - JSX expressions
    - Object properties
    - Template literals
    
    fixes #5015

--- packages/ts-transformer/tests/fixtures/templateLiteral.tsx
+++ packages/ts-transformer/tests/fixtures/templateLiteral.tsx
@@ -8,14 +8,27 @@ defineMessage({
     extra spaces`,
 })
 
+defineMessage({
+  id: `template dedent`,
+  defaultMessage: dedent`dedent Hello World!`,
+})
+
 export default class Foo extends Component {
   render() {
     return (
-      <FormattedMessage
-        id="foo.bar.baz"
-        defaultMessage={`Hello World!`}
-        description="The default message."
-      />
+      <>
+        <FormattedMessage
+          id="foo.bar.baz"
+          defaultMessage={`Hello World!`}
+          description="The default message."
+        />
+        <FormattedMessage
+          id="dedent foo.bar.baz"
+          defaultMessage={dedent`dedent 
+            Hello World!`}
+          description="The default message."
+        />
+      </>
     )
   }
 }
