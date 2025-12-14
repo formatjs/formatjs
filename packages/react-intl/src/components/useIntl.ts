921cc3600f40c2261f877fc71ffb07158901c9a5
longlho <holevietlong@gmail.com>
2025-12-14 04:49:32 +0000

    feat(react-intl)!: convert to ESM (#5283)

--- packages/react-intl/src/components/useIntl.ts
+++ packages/react-intl/src/components/useIntl.ts
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import {IntlShape} from '../types'
-import {invariantIntlContext} from '../utils'
-import {Context} from './injectIntl'
+import {IntlShape} from '../types.js'
+import {invariantIntlContext} from '../utils.js'
+import {Context} from './injectIntl.js'
 
 export default function useIntl(this: void): IntlShape {
   const intl = React.useContext(Context)
