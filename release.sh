274135de0dc4757e25a0d6b4cc1c6ca2b0b1edfc
Long Ho <holevietlong@gmail.com>
2025-12-16 07:13:57 -0500

    build: tweak release script

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ set -e
 
 bazel build :dist
 cp -rf $(bazel cquery --output=files dist)/ release/
+chmod -R +w release/
 # Use `--access=public` to publish new packages with `@formatjs/` scope.
 pushd release
 pnpm install
