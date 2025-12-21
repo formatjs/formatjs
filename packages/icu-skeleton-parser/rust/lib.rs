93a2947b4e11ddbacadeeedc0d23ee0d85ff698f
Long Ho <holevietlong@gmail.com>
2025-12-20 21:20:23 -0500

    build: port the rest of skeleton parser to rust

--- packages/icu-skeleton-parser/rust/lib.rs
+++ packages/icu-skeleton-parser/rust/lib.rs
@@ -1,12 +1,14 @@
 pub mod number_format_options;
 pub mod number_skeleton_token;
+pub mod parser;
 
 // Re-export commonly used types
 pub use number_format_options::{
     ExtendedNumberFormatOptions, NumberFormatNotation, NumberFormatOptions,
     NumberFormatOptionsCompactDisplay, NumberFormatOptionsCurrencyDisplay,
-    NumberFormatOptionsCurrencySign, NumberFormatOptionsSignDisplay,
-    NumberFormatOptionsStyle, NumberFormatOptionsUnitDisplay, RoundingModeType,
-    RoundingPriorityType, TrailingZeroDisplay, UseGroupingString, UseGroupingType,
+    NumberFormatOptionsCurrencySign, NumberFormatOptionsSignDisplay, NumberFormatOptionsStyle,
+    NumberFormatOptionsUnitDisplay, RoundingModeType, RoundingPriorityType, TrailingZeroDisplay,
+    UseGroupingString, UseGroupingType,
 };
 pub use number_skeleton_token::NumberSkeletonToken;
+pub use parser::parse_number_skeleton;
