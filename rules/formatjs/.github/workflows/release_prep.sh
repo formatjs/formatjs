9b3d70c77be9cbf6432e60c2eb067304d8a97daa
Long Ho <holevietlong@gmail.com>
2026-01-05 22:32:59 -0500

    build: fix release-prep.sh

--- rules/formatjs/.github/workflows/release_prep.sh
+++ rules/formatjs/.github/workflows/release_prep.sh
@@ -17,7 +17,7 @@ SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 # Add generated API docs to the release, see https://github.com/bazelbuild/bazel-central-registry/issues/5593
 docs="$(mktemp -d)"; targets="$(mktemp)"
-bazel --output_base="$docs" query --output=label --output_file="$targets" 'kind("starlark_doc_extract rule", //...)'
+bazel --output_base="$docs" query --output=label 'kind("starlark_doc_extract rule", //...)' > "$targets"
 bazel --output_base="$docs" build --target_pattern_file="$targets"
 tar --create --auto-compress \
     --directory "$(bazel --output_base="$docs" info bazel-bin)" \
