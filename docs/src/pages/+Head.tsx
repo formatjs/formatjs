750613c2fc95e21f362a99df5ccc4f92ad6e5912
Long Ho <holevietlong@gmail.com>
2026-01-09 10:53:41 -0500

    docs: add GA

--- docs/src/pages/+Head.tsx
+++ docs/src/pages/+Head.tsx
@@ -8,6 +8,22 @@ export function Head(): React.ReactNode {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/img/logo-icon.svg" type="image/svg+xml" />
       <meta name="theme-color" content="#6f3c97" />
+
+      {/* Google Analytics */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-4H6ZM973P2"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4H6ZM973P2');
+          `,
+        }}
+      />
     </>
   )
 }
