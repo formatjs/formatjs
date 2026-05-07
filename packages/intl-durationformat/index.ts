204f917c1775033539425d6e985ada60bfb0a0cb
Long Ho <holevietlong@gmail.com>
2026-05-07 12:51:56 -0400

    fix(@formatjs/intl-durationformat): export duration format options

--- packages/intl-durationformat/index.ts
+++ packages/intl-durationformat/index.ts
@@ -1,1 +1,12 @@
 export * from '#packages/intl-durationformat/core.js'
+export type {
+  DurationFormatConstructor,
+  DurationFormatLocaleInternalData,
+  DurationFormatOptions,
+  DurationFormatPart,
+  DurationInput,
+  DurationRecord,
+  IntlDurationFormatInternal,
+  RawDurationLocaleData,
+  ResolvedDurationFormatOptions,
+} from '#packages/intl-durationformat/types.js'
